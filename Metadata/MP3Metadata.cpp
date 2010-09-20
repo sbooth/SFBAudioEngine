461601ce5a2631a6bd65c71219e0900b9fa6d780
Stephen F. Booth <me@sbooth.org>
2010-09-19 22:18:23 -0700

    Added optional MP3 layer information

--- Metadata/MP3Metadata.cpp
+++ Metadata/MP3Metadata.cpp
@@ -146,7 +146,34 @@ bool MP3Metadata::ReadMetadata(CFErrorRef *error)
 	if(file.audioProperties()) {
 		TagLib::MPEG::Properties *properties = file.audioProperties();
 		AddAudioPropertiesToDictionary(mMetadata, properties);
-		
+
+		// TODO: Is this too much information?
+#if 0
+		switch(properties->version()) {
+			case TagLib::MPEG::Header::Version1:
+				switch(properties->layer()) {
+					case 1:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-1 Layer I"));		break;
+					case 2:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-1 Layer II"));	break;
+					case 3:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-1 Layer III"));	break;
+				}
+				break;
+			case TagLib::MPEG::Header::Version2:
+				switch(properties->layer()) {
+					case 1:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2 Layer I"));		break;
+					case 2:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2 Layer II"));	break;
+					case 3:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2 Layer III"));	break;
+				}
+				break;
+			case TagLib::MPEG::Header::Version2_5:
+				switch(properties->layer()) {
+					case 1:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2.5 Layer I"));	break;
+					case 2:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2.5 Layer II"));	break;
+					case 3:		CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("MPEG-2.5 Layer III"));	break;
+				}
+				break;
+		}
+#endif
+
 		if(properties->xingHeader() && properties->xingHeader()->totalFrames()) {
 			TagLib::uint frames = properties->xingHeader()->totalFrames();
 			CFNumberRef totalFrames = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &frames);
