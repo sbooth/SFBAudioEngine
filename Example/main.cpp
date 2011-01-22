ac656bd33066ae9b33bfe177962305c63673da0a
Stephen F. Booth <me@sbooth.org>
2011-01-22 12:23:33 -0800

    Added newline at the end of usage

--- Example/main.cpp
+++ Example/main.cpp
@@ -51,7 +51,7 @@ renderingFinished(void					*context,
 int main(int argc, char *argv [])
 {
 	if(1 == argc) {
-		printf("Usage: %s file [file ...]", argv[0]);
+		printf("Usage: %s file [file ...]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	
