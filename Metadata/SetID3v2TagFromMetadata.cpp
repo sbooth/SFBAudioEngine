14c805fe9ccf2826f4f3cee19c4335033e2a2f86
Stephen F. Booth <me@sbooth.org>
2013-09-24 22:04:30 -0400

    Use removeFrames(“APIC”) to avoid a compiler bug/crash

--- Metadata/SetID3v2TagFromMetadata.cpp
+++ Metadata/SetID3v2TagFromMetadata.cpp
@@ -95,26 +95,26 @@ SetID3v2TagFromMetadata(const AudioMetadata& metadata, TagLib::ID3v2::Tag *tag, 
 	tag->setGenre(TagLib::StringFromCFString(metadata.GetGenre()));
 	
 	// Date
-	tag->removeFrames("TDRC");
-	if(metadata.GetReleaseDate()) {
-		/*
-		 The timestamp fields are based on a subset of ISO 8601. When being as
-		 precise as possible the format of a time string is
-		 yyyy-MM-ddTHH:mm:ss (year, "-", month, "-", day, "T", hour (out of
-		 24), ":", minutes, ":", seconds), but the precision may be reduced by
-		 removing as many time indicators as wanted. Hence valid timestamps
-		 are
-		 yyyy, yyyy-MM, yyyy-MM-dd, yyyy-MM-ddTHH, yyyy-MM-ddTHH:mm and
-		 yyyy-MM-ddTHH:mm:ss. All time stamps are UTC. For durations, use
-		 the slash character as described in 8601, and for multiple non-
-		 contiguous dates, use multiple strings, if allowed by the frame
-		 definition.
-		*/
-//		year = CFStringGetIntValue(metadata.GetReleaseDate());
-		auto frame = new TagLib::ID3v2::TextIdentificationFrame("TDRC", TagLib::String::Latin1);
-		frame->setText("");
-		tag->addFrame(frame);
-	}
+//	tag->removeFrames("TDRC");
+//	if(metadata.GetReleaseDate()) {
+//		/*
+//		 The timestamp fields are based on a subset of ISO 8601. When being as
+//		 precise as possible the format of a time string is
+//		 yyyy-MM-ddTHH:mm:ss (year, "-", month, "-", day, "T", hour (out of
+//		 24), ":", minutes, ":", seconds), but the precision may be reduced by
+//		 removing as many time indicators as wanted. Hence valid timestamps
+//		 are
+//		 yyyy, yyyy-MM, yyyy-MM-dd, yyyy-MM-ddTHH, yyyy-MM-ddTHH:mm and
+//		 yyyy-MM-ddTHH:mm:ss. All time stamps are UTC. For durations, use
+//		 the slash character as described in 8601, and for multiple non-
+//		 contiguous dates, use multiple strings, if allowed by the frame
+//		 definition.
+//		*/
+////		year = CFStringGetIntValue(metadata.GetReleaseDate());
+//		auto frame = new TagLib::ID3v2::TextIdentificationFrame("TDRC", TagLib::String::Latin1);
+//		frame->setText("");
+//		tag->addFrame(frame);
+//	}
 
 	// Comment
 	tag->setComment(TagLib::StringFromCFString(metadata.GetComment()));
@@ -386,9 +386,8 @@ SetID3v2TagFromMetadata(const AudioMetadata& metadata, TagLib::ID3v2::Tag *tag, 
 
 	// Album art
 	if(setAlbumArt) {
-		for(auto frame : tag->frameList("APIC"))
-			tag->removeFrame(frame);
-		
+		tag->removeFrames("APIC");
+
 		for(auto attachedPicture : metadata.GetAttachedPictures()) {
 			SFB::CGImageSource imageSource = CGImageSourceCreateWithData(attachedPicture->GetData(), nullptr);
 			if(!imageSource)
