f11c344d24bc4ea815153bb965009e0e1e95af79
Stephen F. Booth <me@sbooth.org>
2013-10-11 22:02:01 -0400

    Added OggOpusMetadata

---
+++ Metadata/OggOpusMetadata.cpp
@@ -0,0 +1,195 @@
+/*
+ *  Copyright (C) 2011, 2012, 2013 Stephen F. Booth <me@sbooth.org>
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of Stephen F. Booth nor the names of its 
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <memory>
+
+#include <taglib/tfilestream.h>
+#include <taglib/speexfile.h>
+
+#include "OggSpeexMetadata.h"
+#include "CFWrapper.h"
+#include "CFErrorUtilities.h"
+#include "AddXiphCommentToDictionary.h"
+#include "SetXiphCommentFromMetadata.h"
+#include "AddAudioPropertiesToDictionary.h"
+
+static void RegisterOggSpeexMetadata() __attribute__ ((constructor));
+static void RegisterOggSpeexMetadata()
+{
+	AudioMetadata::RegisterSubclass<OggSpeexMetadata>();
+}
+
+#pragma mark Static Methods
+
+CFArrayRef OggSpeexMetadata::CreateSupportedFileExtensions()
+{
+	CFStringRef supportedExtensions [] = { CFSTR("spx") };
+	return CFArrayCreate(kCFAllocatorDefault, (const void **)supportedExtensions, 1, &kCFTypeArrayCallBacks);
+}
+
+CFArrayRef OggSpeexMetadata::CreateSupportedMIMETypes()
+{
+	CFStringRef supportedMIMETypes [] = { CFSTR("audio/speex") };
+	return CFArrayCreate(kCFAllocatorDefault, (const void **)supportedMIMETypes, 1, &kCFTypeArrayCallBacks);
+}
+
+bool OggSpeexMetadata::HandlesFilesWithExtension(CFStringRef extension)
+{
+	if(nullptr == extension)
+		return false;
+	
+	if(kCFCompareEqualTo == CFStringCompare(extension, CFSTR("spx"), kCFCompareCaseInsensitive))
+		return true;
+	
+	return false;
+}
+
+bool OggSpeexMetadata::HandlesMIMEType(CFStringRef mimeType)
+{
+	if(nullptr == mimeType)
+		return false;
+	
+	if(kCFCompareEqualTo == CFStringCompare(mimeType, CFSTR("audio/speex"), kCFCompareCaseInsensitive))
+		return true;
+	
+	return false;
+}
+
+AudioMetadata * OggSpeexMetadata::CreateMetadata(CFURLRef url)
+{
+	return new OggSpeexMetadata(url);
+}
+
+#pragma mark Creation and Destruction
+
+OggSpeexMetadata::OggSpeexMetadata(CFURLRef url)
+	: AudioMetadata(url)
+{}
+
+OggSpeexMetadata::~OggSpeexMetadata()
+{}
+
+#pragma mark Functionality
+
+bool OggSpeexMetadata::ReadMetadata(CFErrorRef *error)
+{
+	ClearAllMetadata();
+
+	UInt8 buf [PATH_MAX];
+	if(!CFURLGetFileSystemRepresentation(mURL, false, buf, PATH_MAX))
+		return false;
+	
+	std::unique_ptr<TagLib::FileStream> stream(new TagLib::FileStream((const char *)buf, true));
+	if(!stream->isOpen()) {
+		if(error) {
+			SFB::CFString description = CFCopyLocalizedString(CFSTR("The file “%@” could not be opened for reading."), "");
+			SFB::CFString failureReason = CFCopyLocalizedString(CFSTR("Input/output error"), "");
+			SFB::CFString recoverySuggestion = CFCopyLocalizedString(CFSTR("The file may have been renamed, moved, deleted, or you may not have appropriate permissions."), "");
+
+			*error = CreateErrorForURL(AudioMetadataErrorDomain, AudioMetadataInputOutputError, description, mURL, failureReason, recoverySuggestion);
+		}
+
+		return false;
+	}
+
+	TagLib::Ogg::Speex::File file(stream.get());
+	if(!file.isValid()) {
+		if(nullptr != error) {
+			SFB::CFString description = CFCopyLocalizedString(CFSTR("The file “%@” is not a valid Ogg Speex file."), "");
+			SFB::CFString failureReason = CFCopyLocalizedString(CFSTR("Not an Ogg Speex file"), "");
+			SFB::CFString recoverySuggestion = CFCopyLocalizedString(CFSTR("The file's extension may not match the file's type."), "");
+			
+			*error = CreateErrorForURL(AudioMetadataErrorDomain, AudioMetadataInputOutputError, description, mURL, failureReason, recoverySuggestion);
+		}
+		
+		return false;
+	}
+	
+	CFDictionarySetValue(mMetadata, kPropertiesFormatNameKey, CFSTR("Ogg Speex"));
+	
+	if(file.audioProperties())
+		AddAudioPropertiesToDictionary(mMetadata, file.audioProperties());
+	
+	if(file.tag())
+		AddXiphCommentToDictionary(mMetadata, mPictures, file.tag());
+
+	return true;
+}
+
+bool OggSpeexMetadata::WriteMetadata(CFErrorRef *error)
+{
+	UInt8 buf [PATH_MAX];
+	if(!CFURLGetFileSystemRepresentation(mURL, false, buf, PATH_MAX))
+		return false;
+	
+	std::unique_ptr<TagLib::FileStream> stream(new TagLib::FileStream((const char *)buf));
+	if(!stream->isOpen()) {
+		if(error) {
+			SFB::CFString description = CFCopyLocalizedString(CFSTR("The file “%@” could not be opened for writing."), "");
+			SFB::CFString failureReason = CFCopyLocalizedString(CFSTR("Input/output error"), "");
+			SFB::CFString recoverySuggestion = CFCopyLocalizedString(CFSTR("The file may have been renamed, moved, deleted, or you may not have appropriate permissions."), "");
+
+			*error = CreateErrorForURL(AudioMetadataErrorDomain, AudioMetadataInputOutputError, description, mURL, failureReason, recoverySuggestion);
+		}
+
+		return false;
+	}
+
+	TagLib::Ogg::Speex::File file(stream.get(), false);
+	if(!file.isValid()) {
+		if(error) {
+			SFB::CFString description = CFCopyLocalizedString(CFSTR("The file “%@” is not a valid Ogg Speex file."), "");
+			SFB::CFString failureReason = CFCopyLocalizedString(CFSTR("Not an Ogg Speex file"), "");
+			SFB::CFString recoverySuggestion = CFCopyLocalizedString(CFSTR("The file's extension may not match the file's type."), "");
+			
+			*error = CreateErrorForURL(AudioMetadataErrorDomain, AudioMetadataInputOutputError, description, mURL, failureReason, recoverySuggestion);
+		}
+		
+		return false;
+	}
+	
+	SetXiphCommentFromMetadata(*this, file.tag());
+	
+	if(!file.save()) {
+		if(error) {
+			SFB::CFString description = CFCopyLocalizedString(CFSTR("The file “%@” is not a valid Ogg Speex file."), "");
+			SFB::CFString failureReason = CFCopyLocalizedString(CFSTR("Unable to write metadata"), "");
+			SFB::CFString recoverySuggestion = CFCopyLocalizedString(CFSTR("The file's extension may not match the file's type."), "");
+			
+			*error = CreateErrorForURL(AudioMetadataErrorDomain, AudioMetadataInputOutputError, description, mURL, failureReason, recoverySuggestion);
+		}
+		
+		return false;
+	}
+	
+	MergeChangedMetadataIntoMetadata();
+	
+	return true;
+}
