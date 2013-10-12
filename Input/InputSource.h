b85639fd0783f95ee8d7fa7807bdda123bc31654
Stephen F. Booth <me@sbooth.org>
2013-10-12 14:07:23 -0400

    Added doxygen comments

--- Input/InputSource.h
+++ Input/InputSource.h
@@ -32,76 +32,175 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 
-// ========================================
-// Error Codes
-// ========================================
+/*! @file */
+
+/*! The \c CFErrorRef error domain used by \c InputSource */
 extern const CFStringRef		InputSourceErrorDomain;
 
-enum {
+/*! Possible \c CFErrorRef error codes used by \c InputSource */
+enum InputSourceErrorCodes {
+	/*! File not found */
 	InputSourceFileNotFoundError			= 0,
+	/*! Input/output error */
 	InputSourceInputOutputError				= 1
 };
 
-// ========================================
-// Flags
-// ========================================
-enum {
+
+/*! Flags used in \c InputSource::CreateInputSourceForURL */
+enum InputSourceFlags {
+	/*! Files should be mapped in memory using \c mmap() */
 	InputSourceFlagMemoryMapFiles			= 1 << 0,
+	/*! Files should be fully loaded in memory */
 	InputSourceFlagLoadFilesInMemory		= 1 << 1
 };
 
-// ========================================
-// An abstract class presenting access to a stream of bytes
-// ========================================
+
+/*! An abstract class allowing access to a stream of bytes */
 class InputSource
 {
 
 public:
 
 	// ========================================
-	// Factory methods that return an InputSource for the specified URL, or nullptr on failure
+	/*! @name Factory Methods */
+	//@{
+
+	/*!
+	 * Create a new \c InputSource for the given URL
+	 * @param url The URL
+	 * @param flags Optional flags affecting how \c url is handled
+	 * @param error An optional pointer to a \c CFErrorRef to receive error information
+	 * @return An \c InputSource for the specified URL, or \c nullptr on failure
+	 * @see InputSourceFlags
+	 */
 	static InputSource * CreateInputSourceForURL(CFURLRef url, int flags = 0, CFErrorRef *error = nullptr);
 
+	//@}
+
+
 	// ========================================
-	// Destruction
+	/*! @name Creation and Destruction */
+	// @{
+
+	/*! Destroy this \c InputSource */
 	virtual ~InputSource();
 
-	// This class is non-copyable
+	/*! @cond */
+
+	/*! @internal This class is non-copyable */
 	InputSource(const InputSource& rhs) = delete;
+
+	/*! @internal This class is non-assignable */
 	InputSource& operator=(const InputSource& rhs) = delete;
 
+	/*! @endcond */
+
+	//@}
+
+
 	// ========================================
-	// The URL this source will process
+	/*! @name URL access */
+	//@{
+
+	/*!
+	 * Get the URL for this \c InputSource
+	 * @return The URL for this \c InputSource
+	 */
 	inline CFURLRef GetURL() const							{ return mURL; }
-	
+
+	//@}
+
+
 	// ========================================
-	// Bytestream access (must be implemented by subclasses)
+	/*! @name Opening and Closing */
+	//@{
+
+	/*!
+	 * Open the input for reading
+	 * @param error An optional pointer to a \c CFErrorRef to receive error information
+	 * @return \c true on success, \c false otherwise
+	 */
 	virtual bool Open(CFErrorRef *error = nullptr) = 0;
+
+	/*!
+	 * Close the input
+	 * @param error An optional pointer to a \c CFErrorRef to receive error information
+	 * @return \c true on success, \c false otherwise
+	 */
 	virtual bool Close(CFErrorRef *error = nullptr) = 0;
-	
+
+
+	/*!
+	 * Determine if this \c InputSource is open
+	 * @return \c true if the \c InputSource is open, \c false otherwise
+	 */
 	inline bool IsOpen() const								{ return mIsOpen; }
 
+
 	// ========================================
-	// Returns the number of bytes actually read
+	/*! @name Bytestream access */
+	//@{
+
+	/*!
+	 * Read bytes from the input
+	 * @param buffer The destination buffer
+	 * @param byteCount The maximum number of bytes to read
+	 * @return The number of bytes read
+	 *
+	 */
 	virtual SInt64 Read(void *buffer, SInt64 byteCount) = 0;
+
+	/*!
+	 * Determine if the end of input has been reached
+	 * @return \c true if the end of input has been reached, \c false otherwise
+	 */
 	virtual bool AtEOF() const = 0;
-	
+
+
+	/*!
+	 * Get the current offset in the input
+	 * @return The current offset, in bytes
+	 */
 	virtual SInt64 GetOffset() const = 0;
+
+	/*!
+	 * Get the length of the input
+	 * @return The length of the input, in bytes
+	 */
 	virtual SInt64 GetLength() const = 0;
 
-	// ========================================
-	// Seeking support (optional)
-	virtual bool SupportsSeeking() const					{ return false; }
-	virtual bool SeekToOffset(SInt64 /*offset*/)			{ return false; }
-	
-protected:
-	
-	CFURLRef						mURL;				// The location of the bytes to be read
-	bool							mIsOpen;			// Subclasses should set this to true if Open() is successful
 
-	// ========================================
-	// For subclass use only
+	/*!
+	 * Determine if this \c InputSource is seekable
+	 * @return \c true if this \c InputSource supports seeking, \c false otherwise
+	 */
+	virtual bool SupportsSeeking() const					{ return false; }
+
+	/*!
+	 * Seek to the specified byte offset
+	 * @param offset The desired byte offset
+	 * @return \c true on success, \c false otherwise
+	 */
+	virtual bool SeekToOffset(SInt64 offset)				{ return false; }
+
+	//@}
+
+protected:
+
+	/*! The location of the bytes to be read */
+	CFURLRef mURL;
+
+	/*! Subclasses should set this to true if Open() is successful */
+	bool mIsOpen;
+
+
+	/*! Create a new \c InputSource and initialize \c InputSource::mURL to \c nullptr */
 	InputSource();
+
+	/*!
+	 * Create a new \c InputSource
+	 * @param url The URL to which \c InputSource::mURL will be set
+	 */
 	InputSource(CFURLRef url);
 
 };
