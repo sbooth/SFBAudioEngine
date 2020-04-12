9d81c2f24f625bfcaa5375e6c095323dc6875fca
Stephen F. Booth <me@sbooth.org>
2020-04-12 07:27:43 -0500

    Add Swift refinements

---
+++ Player/SFBAudioPlayer.swift
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
+
+import Foundation
+
+extension SFBAudioPlayer {
+	var positionAndTime: (position: SFBAudioPlayerPlaybackPosition, time: SFBAudioPlayerPlaybackTime) {
+		var position = SFBAudioPlayerPlaybackPosition()
+		var time = SFBAudioPlayerPlaybackTime()
+		__getPlaybackPosition(&position, andTime: &time)
+		return (position: position, time: time)
+	}
+}
