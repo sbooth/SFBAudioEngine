d2a9dc5c08ca4fc37379bf5cc1a96f6e77e1632f
Stephen F. Booth <me@sbooth.org>
2020-04-13 13:19:38 -0500

    Add SFBModuleDecoder

---
+++ Decoders/SFBModuleDecoder.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011 - 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
+
+#import "SFBAudioDecoder+Internal.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+// An SFBAudioDecoder subclass supporting various kinds of Modules (MODs)
+@interface SFBModuleDecoder : SFBAudioDecoder
+@end
+
+NS_ASSUME_NONNULL_END
