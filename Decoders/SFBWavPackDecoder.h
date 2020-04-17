b5fdcef32cec91fb2c6d159aff70142d844d9fab
Stephen F. Booth <me@sbooth.org>
2020-04-17 09:37:45 -0500

    Add WavPack decoder

---
+++ Decoders/SFBWavPackDecoder.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011 - 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
+
+#import "SFBAudioDecoder+Internal.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+// An SFBAudioDecoder subclass supporting WavPack
+@interface SFBWavPackDecoder : SFBAudioDecoder
+@end
+
+NS_ASSUME_NONNULL_END
