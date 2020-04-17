47ec4f9bc79c80e84a6b326d7a2d30960a7934f5
Stephen F. Booth <me@sbooth.org>
2020-04-17 14:40:56 -0500

    Add SFBOggVorbisDecoder

---
+++ Decoders/SFBOggVorbisDecoder.m
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2006 - 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
+
+#import <os/log.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+
+#include <vorbis/vorbisfile.h>
+
+#pragma clang diagnostic pop
+
+#import "SFBOggVorbisDecoder.h"
+
+#import "NSError+SFBURLPresentation.h"
+
+#define MAX_READ_SIZE_FRAMES 2048u
+
+static inline AVAudioFrameCount SFB_min(AVAudioFrameCount a, AVAudioFrameCount b) { return a < b ? a : b; }
+
+static size_t GetChannelLayoutSize(UInt32 numberChannelDescriptions)
+{
+	return offsetof(AudioChannelLayout, mChannelDescriptions) + (numberChannelDescriptions * sizeof(AudioChannelDescription));
+}
+
+static AudioChannelLayout * CreateChannelLayout(UInt32 numberChannelDescriptions)
+{
+	size_t layoutSize = GetChannelLayoutSize(numberChannelDescriptions);
+	AudioChannelLayout *channelLayout = (AudioChannelLayout *)malloc(layoutSize);
+	if(!channelLayout)
+		return NULL;
+
+	memset(channelLayout, 0, layoutSize);
+
+	return channelLayout;
+}
+
+static AudioChannelLayout * CreateChannelLayoutWithLabels(UInt32 numberChannelLabels, ...)
+{
+	assert(numberChannelLabels > 0);
+
+	AudioChannelLayout *channelLayout = CreateChannelLayout(numberChannelLabels);
+	if(!channelLayout)
+		return NULL;
+
+	channelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_UseChannelDescriptions;
+
+	va_list args;
+	va_start(args, numberChannelLabels);
+
+	for(UInt32 i = 0; i < numberChannelLabels; ++i)
+		channelLayout->mChannelDescriptions[i].mChannelLabel = va_arg(args, UInt32);
+
+	va_end(args);
+
+	return channelLayout;
+}
+
+static size_t read_func_callback(void *ptr, size_t size, size_t nmemb, void *datasource)
+{
+	NSCParameterAssert(datasource != NULL);
+
+	SFBOggVorbisDecoder *decoder = (__bridge SFBOggVorbisDecoder *)datasource;
+	NSInteger bytesRead;
+	if(![decoder->_inputSource readBytes:ptr length:(NSInteger)(size * nmemb) bytesRead:&bytesRead error:nil])
+		return 0;
+	return (size_t)bytesRead;
+}
+
+static int seek_func_callback(void *datasource, ogg_int64_t offset, int whence)
+{
+	NSCParameterAssert(datasource != NULL);
+
+	SFBOggVorbisDecoder *decoder = (__bridge SFBOggVorbisDecoder *)datasource;
+
+	switch(whence) {
+		case SEEK_SET:
+			// offset remains unchanged
+			break;
+		case SEEK_CUR: {
+			NSInteger inputSourceOffset;
+			if([decoder->_inputSource getOffset:&inputSourceOffset error:nil])
+				offset += inputSourceOffset;
+			break;
+		}
+		case SEEK_END: {
+			NSInteger inputSourceLength;
+			if([decoder->_inputSource getLength:&inputSourceLength error:nil])
+				offset += inputSourceLength;
+			break;
+		}
+	}
+
+	return ![decoder->_inputSource seekToOffset:offset error:nil];
+}
+
+static long tell_func_callback(void *datasource)
+{
+	NSCParameterAssert(datasource != NULL);
+
+	SFBOggVorbisDecoder *decoder = (__bridge SFBOggVorbisDecoder *)datasource;
+	NSInteger offset;
+	if(![decoder->_inputSource getOffset:&offset error:nil])
+		return -1;
+	return (long)offset;
+}
+
+@interface SFBOggVorbisDecoder ()
+{
+@private
+	OggVorbis_File _vorbisFile;
+}
+@end
+
+@implementation SFBOggVorbisDecoder
+
++ (void)load
+{
+	[SFBAudioDecoder registerSubclass:[self class]];
+}
+
++ (NSSet *)supportedPathExtensions
+{
+	return [NSSet setWithArray:@[@"ogg", @"oga"]];
+}
+
++ (NSSet *)supportedMIMETypes
+{
+	return [NSSet setWithObject:@"audio/ogg-vorbis"];
+}
+
+- (BOOL)openReturningError:(NSError **)error
+{
+	if(![super openReturningError:error])
+		return NO;
+
+	ov_callbacks callbacks = {
+		.read_func = read_func_callback,
+		.seek_func = seek_func_callback,
+		.tell_func = tell_func_callback,
+		.close_func = NULL
+	};
+
+	if(ov_test_callbacks((__bridge void *)self, &_vorbisFile, NULL, 0, callbacks)) {
+		if(error)
+			*error = [NSError SFB_errorWithDomain:SFBAudioDecoderErrorDomain
+											 code:SFBAudioDecoderErrorCodeInputOutput
+					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” is not a valid Ogg Vorbis file.", @"")
+											  url:_inputSource.url
+									failureReason:NSLocalizedString(@"Not an Ogg Vorbis file", @"")
+							   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
+
+		return NO;
+	}
+
+	if(ov_test_open(&_vorbisFile)) {
+		os_log_error(OS_LOG_DEFAULT, "ov_test_open failed");
+
+		if(ov_clear(&_vorbisFile))
+			os_log_error(OS_LOG_DEFAULT, "ov_clear failed");
+
+		return NO;
+	}
+
+	vorbis_info *ovInfo = ov_info(&_vorbisFile, -1);
+	if(!ovInfo) {
+		os_log_error(OS_LOG_DEFAULT, "ov_info failed");
+
+		if(ov_clear(&_vorbisFile))
+			os_log_error(OS_LOG_DEFAULT, "ov_clear failed");
+
+		return NO;
+	}
+
+	AVAudioChannelLayout *channelLayout = nil;
+	switch(ovInfo->channels) {
+			// Default channel layouts from Vorbis I specification section 4.3.9
+			// http://www.xiph.org/vorbis/doc/Vorbis_I_spec.html#x1-800004.3.9
+		case 1:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_Mono];				break;
+		case 2:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_Stereo];			break;
+		case 3:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_AC3_3_0];			break;
+		case 4:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_Quadraphonic];		break;
+		case 5:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_MPEG_5_0_C];		break;
+		case 6:		channelLayout = [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_MPEG_5_1_C];		break;
+		case 7: {
+			AudioChannelLayout *layout = CreateChannelLayoutWithLabels(7,
+																	   kAudioChannelLabel_Left, kAudioChannelLabel_Center, kAudioChannelLabel_Right,
+																	   kAudioChannelLabel_LeftSurround, kAudioChannelLabel_RightSurround, kAudioChannelLabel_CenterSurround,
+																	   kAudioChannelLabel_LFEScreen);
+			if(layout) {
+				channelLayout = [[AVAudioChannelLayout alloc] initWithLayout:layout];
+				free(layout);
+			}
+			break;
+		}
+		case 8: {
+			AudioChannelLayout *layout = CreateChannelLayoutWithLabels(8,
+																	   kAudioChannelLabel_Left, kAudioChannelLabel_Center, kAudioChannelLabel_Right,
+																	   kAudioChannelLabel_LeftSurround, kAudioChannelLabel_RightSurround, kAudioChannelLabel_RearSurroundLeft, kAudioChannelLabel_RearSurroundRight,
+																	   kAudioChannelLabel_LFEScreen);
+			if(layout) {
+				channelLayout = [[AVAudioChannelLayout alloc] initWithLayout:layout];
+				free(layout);
+			}
+			break;
+		}
+	}
+
+	_processingFormat = [[AVAudioFormat alloc] initWithCommonFormat:AVAudioPCMFormatFloat32 sampleRate:ovInfo->rate interleaved:NO channelLayout:channelLayout];
+
+	// Set up the source format
+	AudioStreamBasicDescription sourceStreamDescription = {0};
+
+	sourceStreamDescription.mFormatID			= SFBAudioFormatIDVorbis;
+
+	sourceStreamDescription.mSampleRate			= ovInfo->rate;
+	sourceStreamDescription.mChannelsPerFrame	= (UInt32)ovInfo->channels;
+
+	_sourceFormat = [[AVAudioFormat alloc] initWithStreamDescription:&sourceStreamDescription];
+
+	return YES;
+}
+
+- (BOOL)closeReturningError:(NSError **)error
+{
+	if(ov_clear(&_vorbisFile))
+		os_log_error(OS_LOG_DEFAULT, "ov_clear failed");
+
+	return [super closeReturningError:error];
+}
+
+- (BOOL)isOpen
+{
+	return _vorbisFile.datasource != NULL;
+}
+
+- (AVAudioFramePosition)framePosition
+{
+	return ov_pcm_tell(&_vorbisFile);
+}
+
+- (AVAudioFramePosition)frameLength
+{
+	return ov_pcm_total(&_vorbisFile, -1);
+}
+
+- (BOOL)decodeIntoBuffer:(AVAudioPCMBuffer *)buffer frameLength:(AVAudioFrameCount)frameLength error:(NSError **)error
+{
+	NSParameterAssert(buffer != nil);
+
+	// Reset output buffer data size
+	buffer.frameLength = 0;
+
+	if(![buffer.format isEqual:_processingFormat]) {
+		os_log_debug(OS_LOG_DEFAULT, "-decodeAudio:frameLength:error: called with invalid parameters");
+		return NO;
+	}
+
+	if(frameLength > buffer.frameCapacity)
+		frameLength = buffer.frameCapacity;
+
+	AVAudioFrameCount framesRemaining = frameLength;
+	float **pcm_channels = NULL;
+	int bitstream = 0;
+
+	while(framesRemaining > 0) {
+		// Decode a chunk of samples from the file
+		long framesRead = ov_read_float(&_vorbisFile, &pcm_channels, (int)SFB_min(MAX_READ_SIZE_FRAMES, framesRemaining), &bitstream);
+
+		if(framesRead < 0) {
+			os_log_error(OS_LOG_DEFAULT, "Ogg Vorbis decoding error");
+			return NO;
+		}
+
+		// 0 frames indicates EOS
+		if(framesRead == 0)
+			break;
+
+		// Copy the frames from the decoding buffer to the output buffer
+		float * const *floatChannelData = buffer.floatChannelData;
+		AVAudioChannelCount channelCount = buffer.format.channelCount;
+		for(AVAudioChannelCount channel = 0; channel < channelCount; ++channel) {
+			const float *input = pcm_channels[channel];
+			float *output = floatChannelData[channel] + buffer.frameLength;
+			memcpy(output, input, (size_t)framesRead * sizeof(float));
+		}
+
+		buffer.frameLength += (AVAudioFrameCount)framesRead;
+
+		framesRemaining -= framesRead;
+	}
+
+	return YES;
+}
+
+- (BOOL)seekToFrame:(AVAudioFramePosition)frame error:(NSError **)error
+{
+	NSParameterAssert(frame >= 0);
+	if(ov_pcm_seek(&_vorbisFile, frame)) {
+		os_log_error(OS_LOG_DEFAULT, "Ogg Vorbis seek error");
+		return NO;
+	}
+	return YES;
+}
+
+@end
