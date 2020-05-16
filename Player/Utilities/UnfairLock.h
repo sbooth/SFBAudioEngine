c262bc4d4778842f9baa1d7bba760a88db6aa709
Stephen F. Booth <me@sbooth.org>
2020-05-16 09:27:01 -0500

    Replace the use of synchronous dispatch queues with os_unfair_lock (#126)
    * Use os_unfair_lock instead of a synchronous dispatch queue
    
    * Added UnfairLock.h to project

---
+++ Player/Utilities/UnfairLock.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
+
+#pragma once
+
+#include <os/lock.h>
+
+/*! @file UnfairLock.h @brief A scoped wrapper around @c os_unfair_lock */
+
+/*! @brief \c SFBAudioEngine's encompassing namespace */
+namespace SFB {
+
+	/*! @brief A wrapper around @c os_unfair_lock implementing C++ Lockable*/
+	class UnfairLock
+	{
+	public:
+		// ========================================
+		/*! @name Creation and Destruction */
+		//@{
+
+		/*! @brief Create a new \c UnfairLock */
+		inline UnfairLock() : mLock(OS_UNFAIR_LOCK_INIT) {}
+
+		/*! @cond */
+
+		/*! @internal This class is non-copyable */
+		UnfairLock(const UnfairLock& rhs) = delete;
+
+		/*! @internal This class is non-assignable */
+		UnfairLock& operator=(const UnfairLock& rhs) = delete;
+
+		/*! @endcond */
+
+		//@}
+
+
+		// ========================================
+		/*! @name Lockable */
+		//@{
+
+		/*!@brief Lock the lock. */
+		inline void lock() noexcept 		{ os_unfair_lock_lock(&mLock); }
+
+		/*!
+		 * @brief Free the resources used by this \c RingBuffer
+		 * @note This method is not thread safe.
+		 */
+		inline void unlock() noexcept 		{ os_unfair_lock_unlock(&mLock); }
+
+		/*!
+		 * @brief Attempt to lock the lock.
+		 * @return \c true if the lock was successfully locked, \c false on error
+		 */
+		inline bool try_lock() noexcept 	{ return os_unfair_lock_trylock(&mLock); }
+
+		//@}
+
+	private:
+
+		os_unfair_lock		mLock;					/*!< The primitive lock */
+	};
+
+}
