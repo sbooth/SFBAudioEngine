cf2824e5ed8e86c117c3bec91629d60760b93441
Stephen F. Booth <me@sbooth.org>
2020-10-13 11:53:14 -0500

    Fix spacing

--- Metadata/SFBAudioProperties.swift
+++ Metadata/SFBAudioProperties.swift
@@ -1,7 +1,7 @@
 /*
-* Copyright (c) 2020 Stephen F. Booth <me@sbooth.org>
-* See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
-*/
+ * Copyright (c) 2020 Stephen F. Booth <me@sbooth.org>
+ * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
+ */
 
 import Foundation
 
