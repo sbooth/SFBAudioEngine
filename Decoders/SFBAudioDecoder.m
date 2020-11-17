d1694516b98db6a94774df87c23dbd6efe70e023
Stephen F. Booth <me@sbooth.org>
2020-11-17 13:48:03 -0600

    Align -{open,close}ReturningError: with SFBDSDDecoder

--- Decoders/SFBAudioDecoder.m
+++ Decoders/SFBAudioDecoder.m
@@ -175,27 +175,15 @@ static NSMutableArray *_registeredSubclasses = nil;
 
 - (BOOL)openReturningError:(NSError **)error
 {
-	if(self.isOpen) {
-		os_log_info(gSFBAudioDecoderLog, "-openReturningError: called on a decoder that is already open");
-		return YES;
-	}
-
 	if(!_inputSource.isOpen)
 		return [_inputSource openReturningError:error];
-
 	return YES;
 }
 
 - (BOOL)closeReturningError:(NSError **)error
 {
-	if(!self.isOpen) {
-		os_log_info(gSFBAudioDecoderLog, "-closeReturningError: called on a decoder that isn't open");
-		return YES;
-	}
-
 	if(_inputSource.isOpen)
 		return [_inputSource closeReturningError:error];
-
 	return YES;
 }
 
