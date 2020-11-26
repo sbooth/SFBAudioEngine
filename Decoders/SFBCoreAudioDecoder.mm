d579a8f221c141cb2819b3951e7ffa06632c96e1
Stephen F. Booth <me@sbooth.org>
2020-11-25 21:17:25 -0600

    Move to C++ for easier object management

--- Decoders/SFBCoreAudioDecoder.mm
+++ Decoders/SFBCoreAudioDecoder.mm
@@ -3,61 +3,82 @@
  * See https://github.com/sbooth/SFBAudioEngine/blob/master/LICENSE.txt for license information
  */
 
-@import os.log;
+#import <os/log.h>
 
-@import AudioToolbox;
+#import <memory>
+#import <vector>
+
+#import <AudioToolbox/AudioToolbox.h>
 
 #import "SFBCoreAudioDecoder.h"
 
 #import "NSError+SFBURLPresentation.h"
 #import "SFBCStringForOSType.h"
 
-// ========================================
-// Callbacks
-static OSStatus read_callback(void *inClientData, SInt64 inPosition, UInt32 requestCount, void *buffer, UInt32 *actualCount)
-{
-	NSCParameterAssert(inClientData != NULL);
+template <>
+struct ::std::default_delete<OpaqueAudioFileID> {
+	default_delete() = default;
+	template <class U>
+	constexpr default_delete(default_delete<U>) noexcept {}
+	void operator()(OpaqueAudioFileID *af) const noexcept { /* OSStatus result =*/ AudioFileClose(af); }
+};
 
-	SFBCoreAudioDecoder *decoder = (__bridge SFBCoreAudioDecoder *)inClientData;
+template <>
+struct ::std::default_delete<OpaqueExtAudioFile> {
+	default_delete() = default;
+	template <class U>
+	constexpr default_delete(default_delete<U>) noexcept {}
+	void operator()(OpaqueExtAudioFile *eaf) const noexcept { /* OSStatus result =*/ ExtAudioFileDispose(eaf); }
+};
 
-	NSInteger offset;
-	if(![decoder->_inputSource getOffset:&offset error:nil])
-		return kAudioFileUnspecifiedError;
+namespace {
+	// ========================================
+	// Callbacks
+	OSStatus read_callback(void *inClientData, SInt64 inPosition, UInt32 requestCount, void *buffer, UInt32 *actualCount)
+	{
+		NSCParameterAssert(inClientData != nullptr);
 
-	if(inPosition != offset) {
-		if(!decoder->_inputSource.supportsSeeking || ![decoder->_inputSource seekToOffset:inPosition error:nil])
-			return kAudioFileOperationNotSupportedError;
+		SFBCoreAudioDecoder *decoder = (__bridge SFBCoreAudioDecoder *)inClientData;
+
+		NSInteger offset;
+		if(![decoder->_inputSource getOffset:&offset error:nil])
+			return kAudioFileUnspecifiedError;
+
+		if(inPosition != offset) {
+			if(!decoder->_inputSource.supportsSeeking || ![decoder->_inputSource seekToOffset:inPosition error:nil])
+				return kAudioFileOperationNotSupportedError;
+		}
+
+		NSInteger bytesRead;
+		if(![decoder->_inputSource readBytes:buffer length:requestCount bytesRead:&bytesRead error:nil])
+			return kAudioFileUnspecifiedError;
+
+		*actualCount = (UInt32)bytesRead;
+
+		if(decoder->_inputSource.atEOF)
+			return kAudioFileEndOfFileError;
+
+		return noErr;
 	}
 
-	NSInteger bytesRead;
-	if(![decoder->_inputSource readBytes:buffer length:requestCount bytesRead:&bytesRead error:nil])
-		return kAudioFileUnspecifiedError;
+	SInt64 get_size_callback(void *inClientData)
+	{
+		NSCParameterAssert(inClientData != nullptr);
 
-	*actualCount = (UInt32)bytesRead;
+		SFBCoreAudioDecoder *decoder = (__bridge SFBCoreAudioDecoder *)inClientData;
 
-	if(decoder->_inputSource.atEOF)
-		return kAudioFileEndOfFileError;
-
-	return noErr;
-}
-
-static SInt64 get_size_callback(void *inClientData)
-{
-	NSCParameterAssert(inClientData != NULL);
-
-	SFBCoreAudioDecoder *decoder = (__bridge SFBCoreAudioDecoder *)inClientData;
-
-	NSInteger length;
-	if(![decoder->_inputSource getLength:&length error:nil])
-		return -1;
-	return length;
+		NSInteger length;
+		if(![decoder->_inputSource getLength:&length error:nil])
+			return -1;
+		return length;
+	}
 }
 
 @interface SFBCoreAudioDecoder ()
 {
 @private
-	AudioFileID _af;
-	ExtAudioFileRef _eaf;
+	std::unique_ptr<OpaqueAudioFileID> _af;
+	std::unique_ptr<OpaqueExtAudioFile> _eaf;
 }
 @end
 
@@ -71,28 +92,23 @@ static SInt64 get_size_callback(void *inClientData)
 + (NSSet *)supportedPathExtensions
 {
 	UInt32 size = 0;
-	OSStatus result = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_ReadableTypes, 0, NULL, &size);
+	OSStatus result = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_ReadableTypes, 0, nullptr, &size);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfoSize (kAudioFileGlobalInfo_ReadableTypes) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
 		return [NSSet set];
 	}
 
-	UInt32 *readableTypes = (UInt32 *)malloc(size);
-	if(readableTypes == NULL)
-		return [NSSet set];
+	auto readableTypesCount = size / sizeof(UInt32);
+	std::vector<UInt32> readableTypes(readableTypesCount);
 
-	result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_ReadableTypes, 0, NULL, &size, readableTypes);
+	result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_ReadableTypes, 0, nullptr, &size, &readableTypes[0]);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfo (kAudioFileGlobalInfo_ReadableTypes) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
-		free(readableTypes);
 		return [NSSet set];
 	}
 
 	NSMutableSet *supportedExtensions = [NSMutableSet set];
-
-	UInt32 count = size / sizeof(UInt32);
-	for(UInt32 i = 0; i < count; ++i) {
-		UInt32 type = readableTypes[i];
+	for(UInt32 type : readableTypes) {
 		CFArrayRef extensionsForType = nil;
 		size = sizeof(extensionsForType);
 		result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_ExtensionsForType, sizeof(type), &type, &size, &extensionsForType);
@@ -103,36 +119,29 @@ static SInt64 get_size_callback(void *inClientData)
 			os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfo (kAudioFileGlobalInfo_ExtensionsForType) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
 	}
 
-	free(readableTypes);
-
 	return supportedExtensions;
 }
 
 + (NSSet *)supportedMIMETypes
 {
 	UInt32 size = 0;
-	OSStatus result = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_ReadableTypes, 0, NULL, &size);
+	OSStatus result = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_ReadableTypes, 0, nullptr, &size);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfoSize (kAudioFileGlobalInfo_ReadableTypes) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
 		return [NSSet set];
 	}
 
-	UInt32 *readableTypes = (UInt32 *)malloc(size);
-	if(readableTypes == NULL)
-		return [NSSet set];
+	auto readableTypesCount = size / sizeof(UInt32);
+	std::vector<UInt32> readableTypes(readableTypesCount);
 
-	result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_ReadableTypes, 0, NULL, &size, readableTypes);
+	result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_ReadableTypes, 0, nullptr, &size, &readableTypes[0]);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfo (kAudioFileGlobalInfo_ReadableTypes) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
-		free(readableTypes);
 		return [NSSet set];
 	}
 
 	NSMutableSet *supportedMIMETypes = [NSMutableSet set];
-
-	UInt32 count = size / sizeof(UInt32);
-	for(UInt32 i = 0; i < count; ++i) {
-		UInt32 type = readableTypes[i];
+	for(UInt32 type : readableTypes) {
 		CFArrayRef mimeTypesForType = nil;
 		size = sizeof(mimeTypesForType);
 		result = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_MIMETypesForType, sizeof(type), &type, &size, &mimeTypesForType);
@@ -142,8 +151,6 @@ static SInt64 get_size_callback(void *inClientData)
 		else
 			os_log_error(gSFBAudioDecoderLog, "AudioFileGetGlobalInfo (kAudioFileGlobalInfo_MIMETypesForType) failed: %d '%{public}.4s'", result, SFBCStringForOSType(result));
 	}
-
-	free(readableTypes);
 
 	return supportedMIMETypes;
 }
@@ -167,7 +174,8 @@ static SInt64 get_size_callback(void *inClientData)
 		return NO;
 
 	// Open the input file
-	OSStatus result = AudioFileOpenWithCallbacks((__bridge void *)self, read_callback, NULL, get_size_callback, NULL, 0, &_af);
+	AudioFileID audioFile;
+	OSStatus result = AudioFileOpenWithCallbacks((__bridge void *)self, read_callback, nullptr, get_size_callback, nullptr, 0, &audioFile);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "AudioFileOpenWithCallbacks failed: %d", result);
 
@@ -182,15 +190,12 @@ static SInt64 get_size_callback(void *inClientData)
 		return NO;
 	}
 
-	result = ExtAudioFileWrapAudioFileID(_af, false, &_eaf);
+	auto af = std::unique_ptr<OpaqueAudioFileID>(audioFile);
+
+	ExtAudioFileRef extAudioFile;
+	result = ExtAudioFileWrapAudioFileID(af.get(), false, &extAudioFile);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileWrapAudioFileID failed: %d", result);
-
-		result = AudioFileClose(_af);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-
-		_af = NULL;
 
 		if(error)
 			*error = [NSError SFB_errorWithDomain:SFBAudioDecoderErrorDomain
@@ -203,48 +208,29 @@ static SInt64 get_size_callback(void *inClientData)
 		return NO;
 	}
 
+	auto eaf = std::unique_ptr<OpaqueExtAudioFile>(extAudioFile);
+
 	// Query file format
-	AudioStreamBasicDescription format = {0};
+	AudioStreamBasicDescription format{};
 	UInt32 dataSize = sizeof(format);
-	result = ExtAudioFileGetProperty(_eaf, kExtAudioFileProperty_FileDataFormat, &dataSize, &format);
+	result = ExtAudioFileGetProperty(eaf.get(), kExtAudioFileProperty_FileDataFormat, &dataSize, &format);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileGetProperty (kExtAudioFileProperty_FileDataFormat) failed: %d", result);
-
-		result = ExtAudioFileDispose(_eaf);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "ExtAudioFileDispose failed: %d", result);
-
-		result = AudioFileClose(_af);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-
-		_af = NULL;
-		_eaf = NULL;
-
+		if(error)
+			*error = [NSError errorWithDomain:NSOSStatusErrorDomain code:result userInfo:nil];
 		return NO;
 	}
 
 	// Query channel layout
 	AVAudioChannelLayout *channelLayout = nil;
-	result = ExtAudioFileGetPropertyInfo(_eaf, kExtAudioFileProperty_FileChannelLayout, &dataSize, NULL);
+	result = ExtAudioFileGetPropertyInfo(eaf.get(), kExtAudioFileProperty_FileChannelLayout, &dataSize, nullptr);
 	if(result == noErr) {
 		AudioChannelLayout *layout = (AudioChannelLayout *)malloc(dataSize);
-		result = ExtAudioFileGetProperty(_eaf, kExtAudioFileProperty_FileChannelLayout, &dataSize, layout);
+		result = ExtAudioFileGetProperty(eaf.get(), kExtAudioFileProperty_FileChannelLayout, &dataSize, layout);
 		if(result != noErr) {
 			os_log_error(gSFBAudioDecoderLog, "ExtAudioFileGetProperty (kExtAudioFileProperty_FileChannelLayout) failed: %d", result);
 
 			free(layout);
-
-			result = ExtAudioFileDispose(_eaf);
-			if(result != noErr)
-				os_log_error(gSFBAudioDecoderLog, "ExtAudioFileDispose failed: %d", result);
-
-			result = AudioFileClose(_af);
-			if(result != noErr)
-				os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-
-			_af = NULL;
-			_eaf = NULL;
 
 			if(error)
 				*error = [NSError SFB_errorWithDomain:SFBAudioDecoderErrorDomain
@@ -278,7 +264,7 @@ static SInt64 get_size_callback(void *inClientData)
 		_processingFormat = [[AVAudioFormat alloc] initWithStreamDescription:&format channelLayout:channelLayout];
 	// For Apple Lossless and FLAC convert to packed ints if possible, otherwise high-align
 	else if(format.mFormatID == kAudioFormatAppleLossless || format.mFormatID == kAudioFormatFLAC) {
-		AudioStreamBasicDescription asbd = {0};
+		AudioStreamBasicDescription asbd{};
 
 		asbd.mFormatID			= kAudioFormatLinearPCM;
 		asbd.mFormatFlags		= kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsSignedInteger;
@@ -315,17 +301,6 @@ static SInt64 get_size_callback(void *inClientData)
 	// chance that the initialization of _processingFormat failed. If that happened then attempting to set kExtAudioFileProperty_ClientDataFormat
 	// will segfault
 	if(!_processingFormat) {
-		result = ExtAudioFileDispose(_eaf);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "ExtAudioFileDispose failed: %d", result);
-
-		result = AudioFileClose(_af);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-
-		_af = NULL;
-		_eaf = NULL;
-
 		if(error)
 			*error = [NSError SFB_errorWithDomain:SFBAudioDecoderErrorDomain
 											 code:SFBAudioDecoderErrorCodeInputOutput
@@ -337,21 +312,10 @@ static SInt64 get_size_callback(void *inClientData)
 		return NO;
 	}
 
-	result = ExtAudioFileSetProperty(_eaf, kExtAudioFileProperty_ClientDataFormat, sizeof(AudioStreamBasicDescription), _processingFormat.streamDescription);
+	result = ExtAudioFileSetProperty(eaf.get(), kExtAudioFileProperty_ClientDataFormat, sizeof(AudioStreamBasicDescription), _processingFormat.streamDescription);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileSetProperty (kExtAudioFileProperty_ClientDataFormat) failed: %d", result);
 
-		result = ExtAudioFileDispose(_eaf);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "ExtAudioFileDispose failed: %d", result);
-
-		result = AudioFileClose(_af);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-
-		_af = NULL;
-		_eaf = NULL;
-
 		if(error)
 			*error = [NSError SFB_errorWithDomain:SFBAudioDecoderErrorDomain
 											 code:SFBAudioDecoderErrorCodeInputOutput
@@ -362,38 +326,30 @@ static SInt64 get_size_callback(void *inClientData)
 
 		return NO;
 	}
+
+	_af = std::move(af);
+	_eaf = std::move(eaf);
 
 	return YES;
 }
 
 - (BOOL)closeReturningError:(NSError **)error
 {
-	if(_eaf) {
-		OSStatus result = ExtAudioFileDispose(_eaf);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "ExtAudioFileDispose failed: %d", result);
-		_eaf = NULL;
-	}
-
-	if(_af) {
-		OSStatus result = AudioFileClose(_af);
-		if(result != noErr)
-			os_log_error(gSFBAudioDecoderLog, "AudioFileClose failed: %d", result);
-		_af = NULL;
-	}
+	_eaf.reset();
+	_af.reset();
 
 	return [super closeReturningError:error];
 }
 
 - (BOOL)isOpen
 {
-	return _eaf != NULL;
+	return _eaf != nullptr;
 }
 
 - (AVAudioFramePosition)framePosition
 {
 	SInt64 currentFrame;
-	OSStatus result = ExtAudioFileTell(_eaf, &currentFrame);
+	OSStatus result = ExtAudioFileTell(_eaf.get(), &currentFrame);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileTell failed: %d", result);
 		return SFB_UNKNOWN_FRAME_POSITION;
@@ -405,7 +361,7 @@ static SInt64 get_size_callback(void *inClientData)
 {
 	SInt64 frameLength;
 	UInt32 dataSize = sizeof(frameLength);
-	OSStatus result = ExtAudioFileGetProperty(_eaf, kExtAudioFileProperty_FileLengthFrames, &dataSize, &frameLength);
+	OSStatus result = ExtAudioFileGetProperty(_eaf.get(), kExtAudioFileProperty_FileLengthFrames, &dataSize, &frameLength);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileGetProperty (kExtAudioFileProperty_FileLengthFrames) failed: %d", result);
 		return SFB_UNKNOWN_FRAME_LENGTH;
@@ -427,7 +383,7 @@ static SInt64 get_size_callback(void *inClientData)
 
 	buffer.frameLength = buffer.frameCapacity;
 
-	OSStatus result = ExtAudioFileRead(_eaf, &frameLength, buffer.mutableAudioBufferList);
+	OSStatus result = ExtAudioFileRead(_eaf.get(), &frameLength, buffer.mutableAudioBufferList);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileRead failed: %d", result);
 		return NO;
@@ -441,7 +397,7 @@ static SInt64 get_size_callback(void *inClientData)
 - (BOOL)seekToFrame:(AVAudioFramePosition)frame error:(NSError **)error
 {
 	NSParameterAssert(frame >= 0);
-	OSStatus result = ExtAudioFileSeek(_eaf, frame);
+	OSStatus result = ExtAudioFileSeek(_eaf.get(), frame);
 	if(result != noErr) {
 		os_log_error(gSFBAudioDecoderLog, "ExtAudioFileSeek failed: %d", result);
 		return NO;
