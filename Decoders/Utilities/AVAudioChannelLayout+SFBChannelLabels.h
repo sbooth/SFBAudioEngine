649b8c3390739f7786722771763d8cb05998672b
Stephen F. Booth <me@sbooth.org>
2020-11-27 07:45:06 -0600

    Add documentation

--- Decoders/Utilities/AVAudioChannelLayout+SFBChannelLabels.h
+++ Decoders/Utilities/AVAudioChannelLayout+SFBChannelLabels.h
@@ -7,11 +7,25 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Functions for building channel layouts from channel labels
 @interface AVAudioChannelLayout (SFBChannelLabels)
+/// Returns an initialized \c AVAudioChannelLayout object with the specified channel labels or \c nil on failure
+/// @param count The number of channel labels
 + (nullable instancetype)layoutWithChannelLabels:(AVAudioChannelCount)count, ...;
+/// Returns an initialized \c AVAudioChannelLayout object with the specified channel labels or \c nil on failure
+/// @param channelLabels An array of channel labels
+/// @param count The number of channel labels
 + (nullable instancetype)layoutWithChannelLabels:(AudioChannelLabel *)channelLabels count:(AVAudioChannelCount)count;
+/// Returns an initialized \c AVAudioChannelLayout object with the specified channel labels or \c nil on failure
+/// @param count The number of channel labels
 - (nullable instancetype)initWithChannelLabels:(AVAudioChannelCount)count, ...;
+/// Returns an initialized \c AVAudioChannelLayout object with the specified channel labels or \c nil on failure
+/// @param count The number of channel labels
+/// @param ap A variadic argument list containing \c count \c AudioChannelLabel parameters
 - (nullable instancetype)initWithChannelLabels:(AVAudioChannelCount)count ap:(va_list)ap;
+/// Returns an initialized \c AVAudioChannelLayout object with the specified channel labels or \c nil on failure
+/// @param channelLabels An array of channel labels
+/// @param count The number of channel labels
 - (nullable instancetype)initWithChannelLabels:(AudioChannelLabel *)channelLabels count:(AVAudioChannelCount)count;
 @end
 
