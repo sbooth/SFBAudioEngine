3cb31fea8066058e5c18ea3103ea3cec823c1f5c
Stephen F. Booth <me@sbooth.org>
2020-11-29 09:32:01 -0600

    Add documentation

--- Encoders/SFBAudioEncoder+Internal.h
+++ Encoders/SFBAudioEncoder+Internal.h
@@ -22,6 +22,7 @@ extern os_log_t gSFBAudioEncoderLog;
 	AVAudioFramePosition _estimatedFramesToEncode;
 	NSDictionary *_settings;
 }
+/// Returns the encoder name
 @property (class, nonatomic, readonly) SFBAudioEncoderName encoderName;
 @end
 
@@ -30,15 +31,18 @@ extern os_log_t gSFBAudioEncoderLog;
 @interface SFBAudioEncoder (SFBAudioEncoderSubclassRegistration)
 /// Register a subclass with the default priority (\c 0)
 + (void)registerSubclass:(Class)subclass;
-
 /// Register a subclass with the specified priority
 + (void)registerSubclass:(Class)subclass priority:(int)priority;
 @end
 
 @interface SFBAudioEncoder (SFBAudioEncoderSubclassLookup)
+/// Returns the appropriate \c SFBAudioEncoder subclass for encoding \c url
 + (nullable Class)subclassForURL:(NSURL *)url;
+/// Returns the appropriate \c SFBAudioEncoder subclass for encoding paths with \c extension
 + (nullable Class)subclassForPathExtension:(NSString *)extension;
+/// Returns the appropriate \c SFBAudioEncoder subclass for encoding data of \c mimeType
 + (nullable Class)subclassForMIMEType:(NSString *)mimeType;
+/// Returns the appropriate \c SFBAudioEncoder subclass corresponding to \c encoderName
 + (nullable Class)subclassForEncoderName:(SFBAudioEncoderName)encoderName;
 @end
 
