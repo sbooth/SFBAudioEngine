95d0184c6f43ea50f9ff0af530db8b56d10abefc
Stephen F. Booth <me@sbooth.org>
2020-12-06 22:13:42 -0600

    Documentation

--- Decoders/SFBDSDPCMDecoder.h
+++ Decoders/SFBDSDPCMDecoder.h
@@ -14,8 +14,20 @@ NS_SWIFT_NAME(DSDPCMDecoder) @interface SFBDSDPCMDecoder : NSObject <SFBPCMDecod
 + (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
 
+/// Returns an initialized \c SFBDSDPCMDecoder object for the given URL or \c nil on failure
+/// @param url The URL
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDSDPCMDecoder object for the specified URL, or \c nil on failure
 - (nullable instancetype)initWithURL:(NSURL *)url error:(NSError **)error;
+/// Returns an initialized \c SFBDSDPCMDecoder object for the given input source or \c nil on failure
+/// @param inputSource The input source
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDSDPCMDecoder object for the specified input source, or \c nil on failure
 - (nullable instancetype)initWithInputSource:(SFBInputSource *)inputSource error:(NSError **)error;
+/// Returns an initialized \c SFBDSDPCMDecoder object for the given decoder or \c nil on failure
+/// @param decoder The decoder
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDSDPCMDecoder object for the specified decoder, or \c nil on failure
 - (nullable instancetype)initWithDecoder:(id <SFBDSDDecoding>)decoder error:(NSError **)error NS_DESIGNATED_INITIALIZER;
 
 /// The linear gain applied to the converted DSD samples (default is 6 dBFS)
--- Decoders/SFBDoPDecoder.h
+++ Decoders/SFBDoPDecoder.h
@@ -15,8 +15,20 @@ NS_SWIFT_NAME(DoPDecoder) @interface SFBDoPDecoder : NSObject <SFBPCMDecoding>
 + (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
 
+/// Returns an initialized \c SFBDoPDecoder object for the given URL or \c nil on failure
+/// @param url The URL
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDoPDecoder object for the specified URL, or \c nil on failure
 - (nullable instancetype)initWithURL:(NSURL *)url error:(NSError **)error;
+/// Returns an initialized \c SFBDoPDecoder object for the given input source or \c nil on failure
+/// @param inputSource The input source
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDoPDecoder object for the specified input source, or \c nil on failure
 - (nullable instancetype)initWithInputSource:(SFBInputSource *)inputSource error:(NSError **)error;
+/// Returns an initialized \c SFBDoPDecoder object for the given decoder or \c nil on failure
+/// @param decoder The decoder
+/// @param error An optional pointer to a \c NSError to receive error information
+/// @return An initialized \c SFBDoPDecoder object for the specified decoder, or \c nil on failure
 - (nullable instancetype)initWithDecoder:(id <SFBDSDDecoding>)decoder error:(NSError **)error NS_DESIGNATED_INITIALIZER;
 
 @end
