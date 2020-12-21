17d445ab6d4146926bf44418f4d442b6bbb4cfad
Stephen F. Booth <me@sbooth.org>
2020-12-20 18:34:32 -0600

    Documentation tweaks

--- Device/SFBAudioControl.swift
+++ Device/SFBAudioControl.swift
@@ -8,7 +8,6 @@ import Foundation
 extension AudioControl {
 	/// Returns the control's scope
 	/// - note: This corresponds to `kAudioControlPropertyScope`
-	/// - returns: The control's scope
 	/// - throws: An error if the property could not be retrieved
 	public func scope() throws -> PropertyScope {
 		return AudioObject.PropertyScope(rawValue: UInt32(try getProperty(.controlScope) as UInt))!
@@ -16,7 +15,6 @@ extension AudioControl {
 
 	/// Returns the control's element
 	/// - note: This corresponds to `kAudioControlPropertyElement`
-	/// - returns: The control's element
 	/// - throws: An error if the property could not be retrieved
 	public func element() throws -> PropertyElement {
 		return AudioControl.PropertyElement(try getProperty(.controlElement) as UInt)
