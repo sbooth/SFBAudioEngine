444b660027de936807c9fac014515a6163a98705
Stephen F. Booth <me@sbooth.org>
2020-12-31 12:03:44 -0600

    Update description

--- Device/AudioDevice.swift
+++ Device/AudioDevice.swift
@@ -64,7 +64,7 @@ public class AudioDevice: AudioObject {
 
 	public override var debugDescription: String {
 		do {
-			return "<\(type(of: self)): 0x\(String(objectID, radix: 16, uppercase: false)) \"\(try name())\", \(try isAlive() ? try isRunning() ? "running" : "stopped" : "dead")>"
+			return "<\(type(of: self)): 0x\(String(objectID, radix: 16, uppercase: false)) \"\(try name())\">"
 		}
 		catch {
 			return super.debugDescription
