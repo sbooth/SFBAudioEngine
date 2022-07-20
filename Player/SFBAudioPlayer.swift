1de176be122dfbb03dca51cb80e841f1ec11bdb2
Stephen F. Booth <me@sbooth.org>
2022-07-20 16:27:07 -0500

    Documentation for debugDescription

--- Player/SFBAudioPlayer.swift
+++ Player/SFBAudioPlayer.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2020 - 2021 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2020 - 2022 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
@@ -64,6 +64,7 @@ extension AudioPlayer {
 }
 
 extension AudioPlayer.PlaybackState: CustomDebugStringConvertible {
+	// A textual representation of this instance, suitable for debugging.
 	public var debugDescription: String {
 		switch self {
 		case .playing:
