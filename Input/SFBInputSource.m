e59c6320f696d94f80cdea0ad538bf4bcf19dbab
Stephen F. Booth <me@sbooth.org>
2023-05-26 10:39:45 -0500

    Fix function prototypes

--- Input/SFBInputSource.m
+++ Input/SFBInputSource.m
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010 - 2021 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2010 - 2023 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
@@ -19,7 +19,7 @@ NSErrorDomain const SFBInputSourceErrorDomain = @"org.sbooth.AudioEngine.InputSo
 os_log_t gSFBInputSourceLog = NULL;
 
 static void SFBCreateInputSourceLog(void) __attribute__ ((constructor));
-static void SFBCreateInputSourceLog()
+static void SFBCreateInputSourceLog(void)
 {
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
