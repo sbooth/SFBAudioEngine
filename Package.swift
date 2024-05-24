196cbb6deeaf558d940b7b9e5de92fc80fce53a2
Stephen F. Booth <me@sbooth.org>
2024-05-24 09:22:54 -0500

    Add comments on dependencies

--- Package.swift
+++ Package.swift
@@ -20,22 +20,31 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/sbooth/CXXAudioUtilities", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/AVFAudioExtensions", from: "0.1.0"),
+
 		// Standalone dependencies from source
 		.package(url: "https://github.com/sbooth/CDUMB", from: "2.0.3"),
 		.package(url: "https://github.com/sbooth/CWavPack", from: "5.7.0"),
 		.package(url: "https://github.com/sbooth/CXXMonkeysAudio", from: "10.71.0"),
 		.package(url: "https://github.com/sbooth/CXXTagLib", from: "2.0.1"),
-		// Xiph ecosystem XCFrameworks
-		.package(url: "https://github.com/sbooth/flac-binary-xcframework", from: "0.1.0"),
+
+		// Xiph ecosystem
 		.package(url: "https://github.com/sbooth/ogg-binary-xcframework", from: "0.1.0"),
+		// flac-binary-xcframework requires ogg-binary-xcframework
+		.package(url: "https://github.com/sbooth/flac-binary-xcframework", from: "0.1.0"),
+		// opus-binary-xcframework requires ogg-binary-xcframework
 		.package(url: "https://github.com/sbooth/opus-binary-xcframework", from: "0.1.0"),
-		.package(url: "https://github.com/sbooth/speex-binary-xcframework", from: "0.1.0"),
+		// vorbis-binary-xcframework requires ogg-binary-xcframework
 		.package(url: "https://github.com/sbooth/vorbis-binary-xcframework", from: "0.1.0"),
+		// libspeex does not depend on libogg
+		.package(url: "https://github.com/sbooth/CSpeex", from: "1.2.1"),
+
 		// LGPL bits
 		.package(url: "https://github.com/sbooth/lame-binary-xcframework", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/mpc-binary-xcframework", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/mpg123-binary-xcframework", from: "0.1.0"),
+		// sndfile-binary-xcframework requires ogg-binary-xcframework, flac-binary-xcframework, opus-binary-xcframework, and vorbis-binary-xcframework
 		.package(url: "https://github.com/sbooth/sndfile-binary-xcframework", from: "0.1.0"),
+		// Technically only the TTA *encoder* is LGPL'd but for now the decoder and encoder are packaged together
 		.package(url: "https://github.com/sbooth/tta-cpp-binary-xcframework", from: "0.1.0"),
 	],
 	targets: [
@@ -50,11 +59,11 @@ let package = Package(
 				.product(name: "MAC", package: "CXXMonkeysAudio"),
 				.product(name: "taglib", package: "CXXTagLib"),
 				// Xiph ecosystem
-				.product(name: "FLAC", package: "flac-binary-xcframework"),
 				.product(name: "ogg", package: "ogg-binary-xcframework"),
+				.product(name: "FLAC", package: "flac-binary-xcframework"),
 				.product(name: "opus", package: "opus-binary-xcframework"),
-				.product(name: "speex", package: "speex-binary-xcframework"),
 				.product(name: "vorbis", package: "vorbis-binary-xcframework"),
+				.product(name: "speex", package: "CSpeex"),
 				// LGPL bits
 				.product(name: "lame", package: "lame-binary-xcframework"),
 				.product(name: "mpc", package: "mpc-binary-xcframework"),
