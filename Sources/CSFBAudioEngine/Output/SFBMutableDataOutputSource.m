3b13134602309d497b0033582e2d22289603489f
Stephen F. Booth <me@sbooth.org>
2024-06-01 10:02:54 -0500

    Update copyright notice

--- Sources/CSFBAudioEngine/Output/SFBMutableDataOutputSource.m
+++ Sources/CSFBAudioEngine/Output/SFBMutableDataOutputSource.m
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010 - 2022 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2010-2022 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
