97005890ea9700b3a3b2ad5f512f85466a093c97
Stephen Booth <me@sbooth.org>
2024-12-06 13:49:11 -0600

    Sort registered subclasses in descending order by priority (#396)

--- Sources/CSFBAudioEngine/Decoders/SFBDSDDecoder.m
+++ Sources/CSFBAudioEngine/Decoders/SFBDSDDecoder.m
@@ -366,12 +366,15 @@ static NSMutableArray *_registeredSubclasses = nil;
 	subclassInfo.priority = priority;
 
 	[_registeredSubclasses addObject:subclassInfo];
-	[_registeredSubclasses sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+
+	// N.B. `sortUsingComparator:` sorts in ascending order
+	// To sort the array in descending order the comparator is reversed
+	[_registeredSubclasses sortUsingComparator:^NSComparisonResult(id _Nonnull obj1, id _Nonnull obj2) {
 		int a = ((SFBDSDDecoderSubclassInfo *)obj1).priority;
 		int b = ((SFBDSDDecoderSubclassInfo *)obj2).priority;
-		if(a < b)
+		if(a > b)
 			return NSOrderedAscending;
-		else if(a > b)
+		else if(a < b)
 			return NSOrderedDescending;
 		else
 			return NSOrderedSame;
