3883ff3ab482e8fc0f983cd2c62a69b2bc4f33cd
Stephen F. Booth <me@sbooth.org>
2025-01-03 18:47:27 -0600

    Add `.xcodeproj`

--- Sources/SFBAudioEngine/SFBAudioEngine.swift
+++ Sources/SFBAudioEngine/SFBAudioEngine.swift
@@ -1,7 +1,9 @@
 //
-// Copyright (c) 2006-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2006-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
 
+#if canImport(CSFBAudioEngine)
 @_exported import CSFBAudioEngine
+#endif
