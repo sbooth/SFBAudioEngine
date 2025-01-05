fb8430f50a080cb5e82ddab32b58416ba9bef531
Stephen F. Booth <me@sbooth.org>
2025-01-05 08:27:11 -0600

    Add closing namespace and documentation comments

--- Sources/CSFBAudioEngine/Metadata/SFBAudioMetadata+TagLibMP4Tag.h
+++ Sources/CSFBAudioEngine/Metadata/SFBAudioMetadata+TagLibMP4Tag.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2010-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
@@ -11,13 +11,17 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SFBAudioMetadata (TagLibMP4Tag)
+/// Adds metadata and album art from `tag` to `self`
 - (void)addMetadataFromTagLibMP4Tag:(const TagLib::MP4::Tag *)tag;
 @end
 
 namespace SFB {
-	namespace Audio {
-		void SetMP4TagFromMetadata(SFBAudioMetadata *metadata, TagLib::MP4::Tag *tag, bool setAlbumArt = true);
-	}
-}
+namespace Audio {
+
+/// Sets values in `tag` using `metadata`
+void SetMP4TagFromMetadata(SFBAudioMetadata *metadata, TagLib::MP4::Tag *tag, bool setAlbumArt = true);
+
+} /* namespace Audio */
+} /* namespace SFB */
 
 NS_ASSUME_NONNULL_END
