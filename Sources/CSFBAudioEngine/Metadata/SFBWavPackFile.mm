53acd1747b097bc37f78401213ab83a8630209f9
Stephen F. Booth <me@sbooth.org>
2025-02-13 21:15:36 -0600

    Handle C++ exceptions

--- Sources/CSFBAudioEngine/Metadata/SFBWavPackFile.mm
+++ Sources/CSFBAudioEngine/Metadata/SFBWavPackFile.mm
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2006-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2006-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
@@ -59,99 +59,116 @@ SFBAudioFileFormatName const SFBAudioFileFormatNameWavPack = @"org.sbooth.AudioE
 
 - (BOOL)readPropertiesAndMetadataReturningError:(NSError **)error
 {
-	TagLib::FileStream stream(self.url.fileSystemRepresentation, true);
-	if(!stream.isOpen()) {
+	try {
+		TagLib::FileStream stream(self.url.fileSystemRepresentation, true);
+		if(!stream.isOpen()) {
+			if(error)
+				*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
+												 code:SFBAudioFileErrorCodeInputOutput
+						descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be opened for reading.", @"")
+												  url:self.url
+										failureReason:NSLocalizedString(@"Input/output error", @"")
+								   recoverySuggestion:NSLocalizedString(@"The file may have been renamed, moved, deleted, or you may not have appropriate permissions.", @"")];
+			return NO;
+		}
+
+		TagLib::WavPack::File file(&stream);
+		if(!file.isValid()) {
+			if(error)
+				*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
+												 code:SFBAudioFileErrorCodeInvalidFormat
+						descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” is not a valid WavPack file.", @"")
+												  url:self.url
+										failureReason:NSLocalizedString(@"Not a WavPack file", @"")
+								   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
+			return NO;
+		}
+
+		NSMutableDictionary *propertiesDictionary = [NSMutableDictionary dictionaryWithObject:@"WavPack" forKey:SFBAudioPropertiesKeyFormatName];
+		if(file.audioProperties()) {
+			auto properties = file.audioProperties();
+			SFB::Audio::AddAudioPropertiesToDictionary(properties, propertiesDictionary);
+
+			if(properties->bitsPerSample())
+				propertiesDictionary[SFBAudioPropertiesKeyBitDepth] = @(properties->bitsPerSample());
+			if(properties->sampleFrames())
+				propertiesDictionary[SFBAudioPropertiesKeyFrameLength] = @(properties->sampleFrames());
+		}
+
+		SFBAudioMetadata *metadata = [[SFBAudioMetadata alloc] init];
+		if(file.hasID3v1Tag())
+			[metadata addMetadataFromTagLibID3v1Tag:file.ID3v1Tag()];
+
+		if(file.hasAPETag())
+			[metadata addMetadataFromTagLibAPETag:file.APETag()];
+
+		self.properties = [[SFBAudioProperties alloc] initWithDictionaryRepresentation:propertiesDictionary];
+		self.metadata = metadata;
+
+		return YES;
+	}
+	catch(const std::exception& e) {
+		os_log_error(gSFBAudioFileLog, "Error writing WavPack metadata: %{public}s", e.what());
 		if(error)
-			*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
-											 code:SFBAudioFileErrorCodeInputOutput
-					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be opened for reading.", @"")
-											  url:self.url
-									failureReason:NSLocalizedString(@"Input/output error", @"")
-							   recoverySuggestion:NSLocalizedString(@"The file may have been renamed, moved, deleted, or you may not have appropriate permissions.", @"")];
+			*error = [NSError errorWithDomain:SFBAudioFileErrorDomain code:SFBAudioFileErrorCodeInternalError userInfo:nil];
 		return NO;
 	}
-
-	TagLib::WavPack::File file(&stream);
-	if(!file.isValid()) {
-		if(error)
-			*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
-											 code:SFBAudioFileErrorCodeInvalidFormat
-					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” is not a valid WavPack file.", @"")
-											  url:self.url
-									failureReason:NSLocalizedString(@"Not a WavPack file", @"")
-							   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
-		return NO;
-	}
-
-	NSMutableDictionary *propertiesDictionary = [NSMutableDictionary dictionaryWithObject:@"WavPack" forKey:SFBAudioPropertiesKeyFormatName];
-	if(file.audioProperties()) {
-		auto properties = file.audioProperties();
-		SFB::Audio::AddAudioPropertiesToDictionary(properties, propertiesDictionary);
-
-		if(properties->bitsPerSample())
-			propertiesDictionary[SFBAudioPropertiesKeyBitDepth] = @(properties->bitsPerSample());
-		if(properties->sampleFrames())
-			propertiesDictionary[SFBAudioPropertiesKeyFrameLength] = @(properties->sampleFrames());
-	}
-
-	SFBAudioMetadata *metadata = [[SFBAudioMetadata alloc] init];
-	if(file.hasID3v1Tag())
-		[metadata addMetadataFromTagLibID3v1Tag:file.ID3v1Tag()];
-
-	if(file.hasAPETag())
-		[metadata addMetadataFromTagLibAPETag:file.APETag()];
-
-	self.properties = [[SFBAudioProperties alloc] initWithDictionaryRepresentation:propertiesDictionary];
-	self.metadata = metadata;
-	return YES;
 }
 
 - (BOOL)writeMetadataReturningError:(NSError **)error
 {
-	TagLib::FileStream stream(self.url.fileSystemRepresentation);
-	if(!stream.isOpen()) {
+	try {
+		TagLib::FileStream stream(self.url.fileSystemRepresentation);
+		if(!stream.isOpen()) {
+			if(error)
+				*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
+												 code:SFBAudioFileErrorCodeInputOutput
+						descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be opened for writing.", @"")
+												  url:self.url
+										failureReason:NSLocalizedString(@"Input/output error", @"")
+								   recoverySuggestion:NSLocalizedString(@"The file may have been renamed, moved, deleted, or you may not have appropriate permissions.", @"")];
+			return NO;
+		}
+
+		TagLib::WavPack::File file(&stream, false);
+		if(!file.isValid()) {
+			if(error)
+				*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
+												 code:SFBAudioFileErrorCodeInvalidFormat
+						descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” is not a valid WavPack file.", @"")
+												  url:self.url
+										failureReason:NSLocalizedString(@"Not a WavPack file", @"")
+								   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
+			return NO;
+		}
+
+		// ID3v1 tags are only written if present, but an APE tag is always written
+
+		if(file.hasID3v1Tag())
+			SFB::Audio::SetID3v1TagFromMetadata(self.metadata, file.ID3v1Tag());
+
+		SFB::Audio::SetAPETagFromMetadata(self.metadata, file.APETag(true));
+
+		if(!file.save()) {
+			if(error)
+				*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
+												 code:SFBAudioFileErrorCodeInputOutput
+						descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be saved.", @"")
+												  url:self.url
+										failureReason:NSLocalizedString(@"Unable to write metadata", @"")
+								   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
+			return NO;
+		}
+
+
+		return YES;
+	}
+	catch(const std::exception& e) {
+		os_log_error(gSFBAudioFileLog, "Error writing WavPack metadata: %{public}s", e.what());
 		if(error)
-			*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
-											 code:SFBAudioFileErrorCodeInputOutput
-					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be opened for writing.", @"")
-											  url:self.url
-									failureReason:NSLocalizedString(@"Input/output error", @"")
-							   recoverySuggestion:NSLocalizedString(@"The file may have been renamed, moved, deleted, or you may not have appropriate permissions.", @"")];
+			*error = [NSError errorWithDomain:SFBAudioFileErrorDomain code:SFBAudioFileErrorCodeInternalError userInfo:nil];
 		return NO;
 	}
-
-	TagLib::WavPack::File file(&stream, false);
-	if(!file.isValid()) {
-		if(error)
-			*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
-											 code:SFBAudioFileErrorCodeInvalidFormat
-					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” is not a valid WavPack file.", @"")
-											  url:self.url
-									failureReason:NSLocalizedString(@"Not a WavPack file", @"")
-							   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
-		return NO;
-	}
-
-	// ID3v1 tags are only written if present, but an APE tag is always written
-
-	if(file.hasID3v1Tag())
-		SFB::Audio::SetID3v1TagFromMetadata(self.metadata, file.ID3v1Tag());
-
-	SFB::Audio::SetAPETagFromMetadata(self.metadata, file.APETag(true));
-
-	if(!file.save()) {
-		if(error)
-			*error = [NSError SFB_errorWithDomain:SFBAudioFileErrorDomain
-											 code:SFBAudioFileErrorCodeInputOutput
-					descriptionFormatStringForURL:NSLocalizedString(@"The file “%@” could not be saved.", @"")
-											  url:self.url
-									failureReason:NSLocalizedString(@"Unable to write metadata", @"")
-							   recoverySuggestion:NSLocalizedString(@"The file's extension may not match the file's type.", @"")];
-		return NO;
-	}
-
-
-	return YES;
 }
 
 @end
