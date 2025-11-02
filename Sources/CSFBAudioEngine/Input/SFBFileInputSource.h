11d482a88ea73551f58de2d61fa797404e2f908f
Stephen F. Booth <me@sbooth.org>
2025-11-02 06:48:56 -0600

    Mark `SFBInputSource.new` and `init` as unavailable

--- Sources/CSFBAudioEngine/Input/SFBFileInputSource.h
+++ Sources/CSFBAudioEngine/Input/SFBFileInputSource.h
@@ -1,17 +1,16 @@
 //
-// Copyright (c) 2010-2021 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2010-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
 
-#import "SFBInputSource.h"
+#import "SFBInputSource+Internal.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SFBFileInputSource : SFBInputSource
 + (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
-- (nullable instancetype)initWithURL:(NSURL *)url error:(NSError **)error NS_DESIGNATED_INITIALIZER;
 @end
 
 NS_ASSUME_NONNULL_END
