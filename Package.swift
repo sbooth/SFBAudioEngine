b53dfd25dba3660885db44e22fb01ad1c6375f46
Stephen F. Booth <me@sbooth.org>
2025-11-19 20:00:16 -0600

    Use CXXCoreAudio and CXXAudioToolbox releases

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/sbooth/AVFAudioExtensions", from: "0.4.1"),
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", branch: "main"),
-		.package(url: "https://github.com/sbooth/CXXAudioToolbox", branch: "main"),
+		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.1.0"),
+		.package(url: "https://github.com/sbooth/CXXAudioToolbox", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/CXXRingBuffer", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/CXXUnfairLock", from: "0.1.0"),
 
