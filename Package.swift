318cde4e5b5ed3f0fb32298e7b50cbedd32ed048
Stephen Booth <me@sbooth.org>
2025-11-23 12:31:49 -0600

    Update to Monkey's Audio SDK version 11.82 (#627)

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
 
 		// Standalone dependencies from source
 		.package(url: "https://github.com/sbooth/CDUMB", from: "2.0.3"),
-		.package(url: "https://github.com/sbooth/CXXMonkeysAudio", from: "11.22.0"),
+		.package(url: "https://github.com/sbooth/CXXMonkeysAudio", from: "11.82.0"),
 		.package(url: "https://github.com/sbooth/CXXTagLib", from: "2.1.0"),
 
 		// Standalone dependencies not easily packaged using SPM
