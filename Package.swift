9da5debed09230770db19c97cec2013da0fa1a2a
Stephen Booth <me@sbooth.org>
2025-12-20 22:56:31 -0600

    Update `CXXRingBuffer` and `CXXCoreAudio`

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/sbooth/AVFAudioExtensions", from: "0.4.2"),
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.3.0"),
+		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.3.1"),
 		.package(url: "https://github.com/sbooth/CXXAudioToolbox", from: "0.2.0"),
-		.package(url: "https://github.com/sbooth/CXXRingBuffer", from: "0.3.0"),
+		.package(url: "https://github.com/sbooth/CXXRingBuffer", from: "0.3.1"),
 		.package(url: "https://github.com/sbooth/CXXSharedMutex", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/CXXUnfairLock", from: "0.1.0"),
 
