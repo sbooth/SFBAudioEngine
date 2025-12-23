f5289dddebc241944413a11d30abf0c7bb7ff3c2
Stephen Booth <me@sbooth.org>
2025-12-23 09:20:20 -0600

    Update package version requirements

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/sbooth/AVFAudioExtensions", from: "0.4.2"),
 		.package(url: "https://github.com/sbooth/CXXAudioToolbox", from: "0.2.0"),
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", branch: "alignas"),
-		.package(url: "https://github.com/sbooth/CXXRingBuffer", branch: "alignas"),
+		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.4.0"),
+		.package(url: "https://github.com/sbooth/CXXRingBuffer", from: "0.4.0"),
 		.package(url: "https://github.com/sbooth/CXXSharedMutex", from: "0.1.0"),
 		.package(url: "https://github.com/sbooth/CXXUnfairLock", from: "0.1.0"),
 
