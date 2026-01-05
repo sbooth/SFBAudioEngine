ae931899839151d689380aa9c773784577fe0180
Stephen Booth <me@sbooth.org>
2026-01-05 10:37:14 -0600

    Updates

--- Sources/CSFBAudioEngine/include/SFBAudioEngine/SFBAudioDecoding.h
+++ Sources/CSFBAudioEngine/include/SFBAudioEngine/SFBAudioDecoding.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2006-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright (c) 2006-2026 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/SFBAudioEngine
 // MIT license
 //
