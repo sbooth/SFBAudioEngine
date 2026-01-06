b526bf6e20c5b8c15ef863054c00180cd9f08042
Stephen Booth <me@sbooth.org>
2026-01-06 13:57:10 -0600

    Add nullability specifiers (#752)

--- Sources/CSFBAudioEngine/Utilities/SFBErrorWithLocalizedDescription.h
+++ Sources/CSFBAudioEngine/Utilities/SFBErrorWithLocalizedDescription.h
@@ -9,6 +9,6 @@
 CF_EXTERN_C_BEGIN
 
 /// Returns an `NSError` object with the `NSLocalizedDescriptionKey` set to `format` after formatting according to the current locale
-NSError * SFBErrorWithLocalizedDescription(NSErrorDomain domain, NSInteger code, NSString * _Nonnull format, NSDictionary * _Nullable userInfo, ...) NS_FORMAT_FUNCTION(3,5);
+NSError * _Nonnull SFBErrorWithLocalizedDescription(NSErrorDomain _Nonnull domain, NSInteger code, NSString * _Nonnull format, NSDictionary * _Nullable userInfo, ...) NS_FORMAT_FUNCTION(3,5);
 
 CF_EXTERN_C_END
