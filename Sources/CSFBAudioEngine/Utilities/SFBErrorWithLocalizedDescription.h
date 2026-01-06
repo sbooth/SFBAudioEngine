d8aa363d127fc22e65c09ad1e145cafa6a3b9326
Stephen Booth <me@sbooth.org>
2026-01-06 11:46:44 -0600

    Add `SFBErrorWithLocalizedDescription` and `SFBLocalizedNameForURL`

---
+++ Sources/CSFBAudioEngine/Utilities/SFBErrorWithLocalizedDescription.h
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2026 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SFBAudioEngine
+// MIT license
+//
+
+#import <Foundation/Foundation.h>
+
+CF_EXTERN_C_BEGIN
+
+/// Returns an `NSError` object with the `NSLocalizedDescriptionKey` set to `format` after formatting according to the current locale
+NSError * SFBErrorWithLocalizedDescription(NSErrorDomain domain, NSInteger code, NSString * _Nonnull format, NSDictionary * _Nullable userInfo, ...) NS_FORMAT_FUNCTION(3,5);
+
+CF_EXTERN_C_END
