e2eadd90e497df54e191520fcb5b512ce8d27bea
Stephen Booth <me@sbooth.org>
2026-01-13 11:31:25 -0600

    Prevent UAF in rendering started and complete notifications (#781)
    Fixes #780

---
+++ Sources/CSFBAudioEngine/Player/SFBAudioPlayer+Internal.h
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2006-2026 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SFBAudioEngine
+// MIT license
+//
+
+#import <memory>
+
+#import "SFBAudioPlayer.h"
+#import "AudioPlayer.h"
+
+@interface SFBAudioPlayer ()
+{
+@package
+	/// The underlying AudioPlayer instance
+	SFB::AudioPlayer::unique_ptr _player;
+}
+@end
