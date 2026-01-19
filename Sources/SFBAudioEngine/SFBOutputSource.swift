6df416398e024eacedc3bc0c1f69f7ac4b6067e8
Stephen Booth <me@sbooth.org>
2026-01-19 15:05:42 -0600

    Replace tabs with spaces

--- Sources/SFBAudioEngine/SFBOutputSource.swift
+++ Sources/SFBAudioEngine/SFBOutputSource.swift
@@ -7,118 +7,118 @@
 import Foundation
 
 extension OutputSource {
-	/// Reads bytes from the input
-	/// - parameter buffer: A buffer to receive data
-	/// - parameter length: The maximum number of bytes to read
-	/// - returns: The number of bytes actually read
-	/// - throws: An `NSError` object if an error occurs
-	public func read(_ buffer: UnsafeMutableRawPointer, length: Int) throws -> Int {
-		var bytesRead = 0
-		try __readBytes(buffer, length: length, bytesRead: &bytesRead)
-		return bytesRead
-	}
+    /// Reads bytes from the input
+    /// - parameter buffer: A buffer to receive data
+    /// - parameter length: The maximum number of bytes to read
+    /// - returns: The number of bytes actually read
+    /// - throws: An `NSError` object if an error occurs
+    public func read(_ buffer: UnsafeMutableRawPointer, length: Int) throws -> Int {
+        var bytesRead = 0
+        try __readBytes(buffer, length: length, bytesRead: &bytesRead)
+        return bytesRead
+    }
 
-	/// Writes bytes to the output
-	/// - parameter buffer: A buffer of data to write
-	/// - parameter length: The maximum number of bytes to write
-	/// - returns:The number of bytes actually written
-	/// - throws: An `NSError` object if an error occurs
-	public func write(_ buffer: UnsafeRawPointer, length: Int) throws -> Int {
-		var bytesWritten = 0
-		try __writeBytes(buffer, length: length, bytesWritten: &bytesWritten)
-		return bytesWritten
-	}
+    /// Writes bytes to the output
+    /// - parameter buffer: A buffer of data to write
+    /// - parameter length: The maximum number of bytes to write
+    /// - returns:The number of bytes actually written
+    /// - throws: An `NSError` object if an error occurs
+    public func write(_ buffer: UnsafeRawPointer, length: Int) throws -> Int {
+        var bytesWritten = 0
+        try __writeBytes(buffer, length: length, bytesWritten: &bytesWritten)
+        return bytesWritten
+    }
 
-	/// The current offset in the output, in bytes
-	/// - throws: An `NSError` object if an error occurs
-	public var offset: Int {
-		get throws {
-			var offset = 0
-			try __getOffset(&offset)
-			return offset
-		}
-	}
+    /// The current offset in the output, in bytes
+    /// - throws: An `NSError` object if an error occurs
+    public var offset: Int {
+        get throws {
+            var offset = 0
+            try __getOffset(&offset)
+            return offset
+        }
+    }
 
-	/// The length of the output, in bytes
-	/// - throws: An `NSError` object if an error occurs
-	public var length: Int {
-		get throws {
-			var length = 0
-			try __getLength(&length)
-			return length
-		}
-	}
+    /// The length of the output, in bytes
+    /// - throws: An `NSError` object if an error occurs
+    public var length: Int {
+        get throws {
+            var length = 0
+            try __getLength(&length)
+            return length
+        }
+    }
 
-	/// Writes bytes to the output
-	/// - parameter data: The data to write
-	/// - throws: An `NSError` object if an error occurs
-	public func write(_ data: Data) throws {
-		let bytesWritten = try data.withUnsafeBytes { (bufptr) -> Int in
-			guard let baseAddress = bufptr.baseAddress else {
-				return 0
-			}
-			return try write(baseAddress, length: data.count)
-		}
+    /// Writes bytes to the output
+    /// - parameter data: The data to write
+    /// - throws: An `NSError` object if an error occurs
+    public func write(_ data: Data) throws {
+        let bytesWritten = try data.withUnsafeBytes { (bufptr) -> Int in
+            guard let baseAddress = bufptr.baseAddress else {
+                return 0
+            }
+            return try write(baseAddress, length: data.count)
+        }
 
-		if bytesWritten != data.count {
-			throw NSError(domain: NSPOSIXErrorDomain, code: Int(EIO), userInfo: nil)
-		}
-	}
+        if bytesWritten != data.count {
+            throw NSError(domain: NSPOSIXErrorDomain, code: Int(EIO), userInfo: nil)
+        }
+    }
 
-	/// Writes a binary integer to the output
-	/// - parameter i: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func write<T: BinaryInteger>(_ i: T) throws {
-		let size = MemoryLayout<T>.size
+    /// Writes a binary integer to the output
+    /// - parameter i: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func write<T: BinaryInteger>(_ i: T) throws {
+        let size = MemoryLayout<T>.size
 
-		let bytesWritten = try withUnsafePointer(to: i) {
-			return try write($0, length: size)
-		}
+        let bytesWritten = try withUnsafePointer(to: i) {
+            return try write($0, length: size)
+        }
 
-		if bytesWritten != size {
-			throw NSError(domain: NSPOSIXErrorDomain, code: Int(EIO), userInfo: nil)
-		}
-	}
+        if bytesWritten != size {
+            throw NSError(domain: NSPOSIXErrorDomain, code: Int(EIO), userInfo: nil)
+        }
+    }
 
-	/// Writes a 16-bit integer to the output in big-endian format
-	/// - parameter ui16: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeBigEndian(_ ui16: UInt16) throws {
-		try write(CFSwapInt16HostToBig(ui16))
-	}
+    /// Writes a 16-bit integer to the output in big-endian format
+    /// - parameter ui16: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeBigEndian(_ ui16: UInt16) throws {
+        try write(CFSwapInt16HostToBig(ui16))
+    }
 
-	/// Writes a 32-bit integer to the output in big-endian format
-	/// - parameter ui32: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeBigEndian(_ ui32: UInt32) throws {
-		try write(CFSwapInt32HostToBig(ui32))
-	}
+    /// Writes a 32-bit integer to the output in big-endian format
+    /// - parameter ui32: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeBigEndian(_ ui32: UInt32) throws {
+        try write(CFSwapInt32HostToBig(ui32))
+    }
 
-	/// Writes a 64-bit integer to the output in little-endian format
-	/// - parameter ui64: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeBigEndian(_ ui64: UInt64) throws {
-		try write(CFSwapInt64HostToBig(ui64))
-	}
+    /// Writes a 64-bit integer to the output in little-endian format
+    /// - parameter ui64: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeBigEndian(_ ui64: UInt64) throws {
+        try write(CFSwapInt64HostToBig(ui64))
+    }
 
-	/// Writes a 16-bit integer to the output in big-endian format
-	/// - parameter ui16: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeLittleEndian(_ ui16: UInt16) throws {
-		try write(CFSwapInt16HostToLittle(ui16))
-	}
+    /// Writes a 16-bit integer to the output in big-endian format
+    /// - parameter ui16: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeLittleEndian(_ ui16: UInt16) throws {
+        try write(CFSwapInt16HostToLittle(ui16))
+    }
 
-	/// Writes a 32-bit integer to the output in little-endian format
-	/// - parameter ui32: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeLittleEndian(_ ui32: UInt32) throws {
-		try write(CFSwapInt32HostToLittle(ui32))
-	}
+    /// Writes a 32-bit integer to the output in little-endian format
+    /// - parameter ui32: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeLittleEndian(_ ui32: UInt32) throws {
+        try write(CFSwapInt32HostToLittle(ui32))
+    }
 
-	/// Writes a 64-bit integer to the output in little-endian format
-	/// - parameter ui64: The value to write
-	/// - throws: An `NSError` object if an error occurs
-	public func writeLittleEndian(_ ui64: UInt64) throws {
-		try write(CFSwapInt64HostToLittle(ui64))
-	}
+    /// Writes a 64-bit integer to the output in little-endian format
+    /// - parameter ui64: The value to write
+    /// - throws: An `NSError` object if an error occurs
+    public func writeLittleEndian(_ ui64: UInt64) throws {
+        try write(CFSwapInt64HostToLittle(ui64))
+    }
 }
--- Sources/SFBAudioEngine/SFBPlaybackTime.swift
+++ Sources/SFBAudioEngine/SFBPlaybackTime.swift
@@ -7,41 +7,41 @@
 import Foundation
 
 extension PlaybackTime {
-	/// Returns `true` if the current time and total time are valid
-	public var isValid: Bool {
-		currentTime != unknownTime && totalTime != unknownTime
-	}
-	/// Returns `true` if the current time is valid
-	public var isCurrentTimeValid: Bool {
-		currentTime != unknownTime
-	}
-	/// Returns `true` if the total time is valid
-	public var isTotalTimeValid: Bool {
-		totalTime != unknownTime
-	}
+    /// Returns `true` if the current time and total time are valid
+    public var isValid: Bool {
+        currentTime != unknownTime && totalTime != unknownTime
+    }
+    /// Returns `true` if the current time is valid
+    public var isCurrentTimeValid: Bool {
+        currentTime != unknownTime
+    }
+    /// Returns `true` if the total time is valid
+    public var isTotalTimeValid: Bool {
+        totalTime != unknownTime
+    }
 
-	/// The current time or `nil` if unknown
-	public var current: TimeInterval? {
-		isCurrentTimeValid ? currentTime : nil
-	}
-	/// The total time or `nil` if unknown
-	public var total: TimeInterval? {
-		isTotalTimeValid ? totalTime : nil
-	}
+    /// The current time or `nil` if unknown
+    public var current: TimeInterval? {
+        isCurrentTimeValid ? currentTime : nil
+    }
+    /// The total time or `nil` if unknown
+    public var total: TimeInterval? {
+        isTotalTimeValid ? totalTime : nil
+    }
 
-	/// Returns `current` as a fraction of `total`
-	public var progress: Double? {
-		guard isValid else {
-			return nil
-		}
-		return currentTime / totalTime
-	}
+    /// Returns `current` as a fraction of `total`
+    public var progress: Double? {
+        guard isValid else {
+            return nil
+        }
+        return currentTime / totalTime
+    }
 
-	/// Returns the time remaining
-	public var remaining: TimeInterval? {
-		guard isValid else {
-			return nil
-		}
-		return totalTime - currentTime
-	}
+    /// Returns the time remaining
+    public var remaining: TimeInterval? {
+        guard isValid else {
+            return nil
+        }
+        return totalTime - currentTime
+    }
 }
