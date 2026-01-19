63d39bcae322c5d222e559530a5d66287b3c57f5
Stephen Booth <me@sbooth.org>
2026-01-19 15:12:20 -0600

    Replace tabs with spaces (#795)

--- Sources/SFBAudioEngine/SFBAudioProperties.swift
+++ Sources/SFBAudioEngine/SFBAudioProperties.swift
@@ -7,32 +7,32 @@
 import Foundation
 
 extension AudioProperties {
-	/// The total number of audio frames
-	public var frameLength: AVAudioFramePosition? {
-		__frameLength?.int64Value
-	}
-	/// The number of channels
-	public var channelCount: AVAudioChannelCount? {
-		__channelCount?.uint32Value
-	}
+    /// The total number of audio frames
+    public var frameLength: AVAudioFramePosition? {
+        __frameLength?.int64Value
+    }
+    /// The number of channels
+    public var channelCount: AVAudioChannelCount? {
+        __channelCount?.uint32Value
+    }
 
-	/// The audio bit depth
-	public var bitDepth: Int? {
-		__bitDepth?.intValue
-	}
+    /// The audio bit depth
+    public var bitDepth: Int? {
+        __bitDepth?.intValue
+    }
 
-	/// The sample rate in Hz
-	public var sampleRate: Double? {
-		__sampleRate?.doubleValue
-	}
+    /// The sample rate in Hz
+    public var sampleRate: Double? {
+        __sampleRate?.doubleValue
+    }
 
-	/// The duration in seconds
-	public var duration: TimeInterval? {
-		__duration?.doubleValue
-	}
+    /// The duration in seconds
+    public var duration: TimeInterval? {
+        __duration?.doubleValue
+    }
 
-	/// The audio bitrate in KiB/sec
-	public var bitrate: Double? {
-		__bitrate?.doubleValue
-	}
+    /// The audio bitrate in KiB/sec
+    public var bitrate: Double? {
+        __bitrate?.doubleValue
+    }
 }
