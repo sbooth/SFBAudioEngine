cbc45f2ff16c4b0bbb4b8829ac1e179fb5b2b79b
Stephen Booth <me@sbooth.org>
2026-01-24 22:16:04 -0600

    Compare ObjC object to nil vs nullptr

--- Sources/CSFBAudioEngine/Decoders/SFBCoreAudioDecoder.mm
+++ Sources/CSFBAudioEngine/Decoders/SFBCoreAudioDecoder.mm
@@ -407,7 +407,7 @@ SInt64 getSizeCallback(void *inClientData) noexcept {
     // For audio with more than 2 channels AVAudioFormat requires a channel map. Since ExtAudioFile doesn't always
     // return one, there is a chance that the initialization of _processingFormat failed. If that happened then
     // attempting to set kExtAudioFileProperty_ClientDataFormat will segfault
-    if (_processingFormat == nullptr) {
+    if (_processingFormat == nil) {
         if (error != nullptr) {
             *error = SFBErrorWithLocalizedDescription(
                   SFBAudioDecoderErrorDomain, SFBAudioDecoderErrorCodeInvalidFormat,
