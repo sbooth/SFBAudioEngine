64d1c04fd0a3f70f85c305f685d8458a095256e8
Stephen Booth <me@sbooth.org>
2026-02-01 09:44:02 -0600

    Selectively disable formatting

--- Sources/CSFBAudioEngine/Utilities/SFBTernaryTruthValue.h
+++ Sources/CSFBAudioEngine/Utilities/SFBTernaryTruthValue.h
@@ -8,13 +8,17 @@
 
 #import <CoreFoundation/CoreFoundation.h>
 
+// clang-format off
+
 /// A three-valued logic truth value (AKA tribool)
 /// No assumption is made whether unknown implies true
 typedef CF_CLOSED_ENUM(NSInteger, SFBTernaryTruthValue) {
     /// True
     SFBTernaryTruthValueTrue CF_SWIFT_NAME(ternaryTrue) = 1,
-            /// False
-            SFBTernaryTruthValueFalse CF_SWIFT_NAME(ternaryFalse) = 0,
-                                                    /// Unknown
-                                                    SFBTernaryTruthValueUnknown CF_SWIFT_NAME(ternaryUnknown) = -1,
-            } CF_SWIFT_NAME(TernaryTruthValue);
+    /// False
+    SFBTernaryTruthValueFalse CF_SWIFT_NAME(ternaryFalse) = 0,
+    /// Unknown
+    SFBTernaryTruthValueUnknown CF_SWIFT_NAME(ternaryUnknown) = -1,
+} CF_SWIFT_NAME(TernaryTruthValue);
+
+// clang-format on
