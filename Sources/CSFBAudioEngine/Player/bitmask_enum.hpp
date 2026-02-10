71db86cbfb495ee4901de08111ad6a8999059d82
Stephen Booth <me@sbooth.org>
2026-02-10 15:05:22 -0600

    Add `has_none` and `has_flag_but_not`

--- Sources/CSFBAudioEngine/Player/bitmask_enum.hpp
+++ Sources/CSFBAudioEngine/Player/bitmask_enum.hpp
@@ -41,19 +41,25 @@ template <typename T>
 concept BitmaskEnum =
         std::is_enum_v<T> && std::is_unsigned_v<std::underlying_type_t<T>> && requires(T t) { is_bitmask_enum(t); };
 
-/// Returns true if all bits in mask are set in value
+/// Returns true if all non-zero bits in mask are set in value
 template <BitmaskEnum E> constexpr bool has_all(E value, E mask) noexcept {
     using U = std::underlying_type_t<E>;
     return (static_cast<U>(value) & static_cast<U>(mask)) == static_cast<U>(mask);
 }
 
-/// Returns true if at least one bit in mask is set in value
+/// Returns true if at least one non-zero bit in mask is set in value
 template <BitmaskEnum E> constexpr bool has_any(E value, E mask) noexcept {
     using U = std::underlying_type_t<E>;
     return (static_cast<U>(value) & static_cast<U>(mask)) != 0;
 }
 
-/// Returns true if none of the bits are set in value
+/// Returns true if all non-zero bits in mask are clear in value
+template <BitmaskEnum E> constexpr bool has_none(E value, E mask) noexcept {
+    using U = std::underlying_type_t<E>;
+    return (static_cast<U>(value) & static_cast<U>(mask)) == 0;
+}
+
+/// Returns true if all bits in value are clear
 template <BitmaskEnum E> constexpr bool is_empty(E value) noexcept {
     return static_cast<std::underlying_type_t<E>>(value) == 0;
 }
@@ -65,26 +71,14 @@ template <BitmaskEnum E> constexpr bool is_single_bit(E value) noexcept {
     return v != 0 && (v & (v - 1)) == 0;
 }
 
-/// Returns true if all bits in flag are set in value
+/// Returns true if all non-zero bits in flag are set in value
 template <BitmaskEnum E> constexpr bool has_flag(E value, E flag) noexcept { return has_all(value, flag); }
 
-/// Returns true if the mask bits from value match expected
-template <BitmaskEnum E> constexpr bool masked_matches(E value, E mask, E expected) noexcept {
+/// Returns true if the non-zero bits from set are set in value and the non-zero bits from clear are clear in value
+template <BitmaskEnum E> constexpr bool has_flag_but_not(E value, E set, E clear) noexcept {
     using U = std::underlying_type_t<E>;
-    assert((static_cast<U>(expected) & ~static_cast<U>(mask)) == 0 && "expected bits must be a subset of mask bits");
-    return (static_cast<U>(value) & static_cast<U>(mask)) == static_cast<U>(expected);
-}
-
-/// Returns value with the specified bits turned off
-template <BitmaskEnum E> constexpr E remove_mask(E value, E mask) noexcept {
-    using U = std::underlying_type_t<E>;
-    return static_cast<E>(static_cast<U>(value) & ~static_cast<U>(mask));
-}
-
-/// Returns value with the specified bits toggled
-template <BitmaskEnum E> constexpr E toggle_mask(E value, E mask) noexcept {
-    using U = std::underlying_type_t<E>;
-    return static_cast<E>(static_cast<U>(value) ^ static_cast<U>(mask));
+    assert((static_cast<U>(set) & static_cast<U>(clear)) == 0 && "on and off bits may not overlap");
+    return (static_cast<U>(value) & (static_cast<U>(set) | static_cast<U>(clear))) == static_cast<U>(set);
 }
 
 } /* namespace bits */
