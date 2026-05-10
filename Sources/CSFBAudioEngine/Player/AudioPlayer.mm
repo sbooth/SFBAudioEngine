17a97b3c8199bf9e9decd9340102ecc94eee9743
Stephen Booth <me@sbooth.org>
2026-05-10 09:45:17 -0600

    Stop `AVAudioEngine` when the configuration changes (#907)

--- Sources/CSFBAudioEngine/Player/AudioPlayer.mm
+++ Sources/CSFBAudioEngine/Player/AudioPlayer.mm
@@ -2212,7 +2212,7 @@ void sfb::AudioPlayer::handleAudioEngineConfigurationChange(AVAudioEngine *engin
         std::unique_lock lock{engineMutex_};
 
         // AVAudioEngine stops itself when a configuration change occurs
-        // Flags::engineIsRunning indicates if the engine was running before the interruption
+        // Flags::engineIsRunning indicates if the engine was running before the configuration change
         const auto prevFlags = clearFlags(Flags::engineIsRunning | Flags::isPlaying);
         const auto prevState = prevFlags & (Flags::engineIsRunning | Flags::isPlaying);
 
@@ -2243,6 +2243,17 @@ void sfb::AudioPlayer::handleAudioEngineConfigurationChange(AVAudioEngine *engin
             os_log_debug(log_, "Setting main mixer → output node connection format to %{public}@",
                          stringDescribingAVAudioFormat(outputNodeOutputFormat));
 #endif /* DEBUG */
+
+            // AVAudioEngine stops itself when a configuration change occurs but it could have been restarted
+            // before the notification was delivered or the lock was acquired.
+            // Disconnecting the main mixer node from the output node when the engine is running causes an exception
+            // so ensure the engine is stopped before updating the bus format.
+            if (engine_.isRunning) {
+#if DEBUG
+                assert(bits::is_set(prevState, Flags::engineIsRunning));
+#endif /* DEBUG */
+                [engine_ stop];
+            }
 
             [engine_ disconnectNodeInput:outputNode bus:0];
 
