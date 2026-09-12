4af17ee9531daffc388e424edb7445098f190c30
Stephen Booth <me@sbooth.org>
2026-09-12 10:10:14 -0500

    Improve accuracy

--- Sources/CSFBAudioEngine/Player/AudioPlayer.mm
+++ Sources/CSFBAudioEngine/Player/AudioPlayer.mm
@@ -49,6 +49,9 @@ constexpr int64_t twoPointFiveMillisecondDispatchTimeDelta = 2'500'000;
 /// 7.5 millisecond dispatch time delta, expressed in nanoseconds
 constexpr int64_t sevenPointFiveMillisecondDispatchTimeDelta = 7'500'000;
 
+/// The closest double value to 2/3
+constexpr double twoThirds = 0x1.5555555555555p-1;
+
 /// Objective-C associated object key indicating if a decoder has been canceled
 constexpr char decoderIsCanceledKey = '\0';
 
@@ -1759,7 +1762,7 @@ void sfb::AudioPlayer::processEvents(std::stop_token stoken) noexcept {
 #if DEBUG
         if (const auto occupancy =
                     static_cast<double>(events_.occupiedSlots()) / static_cast<double>(events_.slotCount);
-            occupancy > 0.66) {
+            occupancy > twoThirds) {
             os_log_debug(log_, "Less than 1/3 headroom in event message queue: occupancy %.2f", occupancy);
         }
 #endif /* DEBUG */
