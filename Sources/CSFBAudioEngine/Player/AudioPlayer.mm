e3e060da0d5962af78dfb2dbaf3398d0e2196592
Stephen Booth <me@sbooth.org>
2026-09-14 08:40:10 -0500

    Debug logging for metadata queue headroom

--- Sources/CSFBAudioEngine/Player/AudioPlayer.mm
+++ Sources/CSFBAudioEngine/Player/AudioPlayer.mm
@@ -1488,6 +1488,14 @@ bool sfb::AudioPlayer::decodeIntoRingBuffer(DecoderState *decoderState, AVAudioP
         return true;
     }
 
+#if DEBUG
+    if (const auto occupancy =
+                static_cast<double>(audioMetadata_.availableToRead()) / static_cast<double>(audioMetadata_.capacity);
+        occupancy > twoThirds) {
+        os_log_debug(log_, "Less than 1/3 headroom in metadata queue: occupancy %.2f", occupancy);
+    }
+#endif /* DEBUG */
+
     // Decode and write chunks and metadata to the ring buffers
     while (audioBuffer_.availableToWrite() >= ringBufferChunkSize && !audioMetadata_.isFull()) {
         // The chunk descriptor for the chunk to be decoded
